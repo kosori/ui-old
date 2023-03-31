a0c100556b07c3ffdf0822c38134cf3c3270da74
Alexis Guzman <codingcodax@gmail.com>
2023-03-31 11:19:55 -0600

    fix(app): remove api import

--- apps/www/src/pages/_app.tsx
+++ apps/www/src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css';
 import type { AppType } from 'next/app';
 
-import { api } from '~/utils/api';
-
 // eslint-disable-next-line react/prop-types
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
