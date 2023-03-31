94d2c12c26934d7368b438ed51eef1682577edfe
Alexis Guzman <codingcodax@gmail.com>
2023-03-31 09:51:57 -0600

    feat(eslint): extends react, prettier and tailwindcss

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,29 +1,53 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: [
-    'next',
-    'turbo',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
+    "next",
+    "turbo",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
   ],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
       {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
       },
     ],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
     ],
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: {
+          // Allow `void` return type for JSX attributes such as `onClick` / `onSubmit`
+          attributes: false,
+        },
+      },
+    ],
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+    "tailwindcss/no-custom-classname": "off",
   },
-  ignorePatterns: ['**/*.config.js', '**/*.config.cjs', 'packages/config/**'],
+  ignorePatterns: ["**/*.config.js", "**/*.config.cjs", "packages/config/**"],
   reportUnusedDisableDirectives: true,
 };
 
