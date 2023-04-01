0b8da53173deed3d62882b36874311a018087a7c
Alexis Guzman <codingcodax@gmail.com>
2023-03-31 20:15:12 -0600

    feat(app): set inter font

--- apps/www/src/pages/_app.tsx
+++ apps/www/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import type { AppType } from 'next/app';
+import { Inter as FontSans } from 'next/font/google';
 
 import '~/styles/globals.css';
 
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
 // eslint-disable-next-line react/prop-types
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/*eslint-disable-next-line react/no-unknown-property*/}
+      <style global jsx>{`
+        :root {
+          --font-sans: ${fontSans.style.fontFamily};
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
