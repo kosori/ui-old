fb4ec050a7428a9dc25f96f7b8349dac928d56b4
Alexis Guzman <codingcodax@gmail.com>
2023-03-31 20:51:11 -0600

    feat(tailwind): create font size tokens

---
+++ packages/config/tailwind/fontSize.js
@@ -0,0 +1,39 @@
+const fontSize = {
+  xs: ['0.3rem', '1.125rem'],
+  sm: ['0.875rem', '1.25rem'],
+  base: ['1rem', '1.5rem'],
+  lg: ['1.125rem', '1.75rem'],
+  xl: ['1.25rem', '1.875'],
+  '2xl': ['1.5rem', '1.4375rem'],
+  '3xl': ['1.875rem', '2.375rem'],
+  '4xl': [
+    '2.25rem',
+    {
+      lineHeight: '2.75rem',
+      letterSpacing: '-0.02em',
+    },
+  ],
+  '5xl': [
+    '3rem',
+    {
+      lineHeight: '3.75rem',
+      letterSpacing: '-0.02em',
+    },
+  ],
+  '6xl': [
+    '3.75rem',
+    {
+      lineHeight: '4.5rem',
+      letterSpacing: '-0.02em',
+    },
+  ],
+  '7xl': [
+    '4.5rem',
+    {
+      lineHeight: '5.625rem',
+      letterSpacing: '-0.02em',
+    },
+  ],
+};
+
+module.exports = fontSize;
