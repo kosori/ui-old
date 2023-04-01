7a15329d9921f66525722aa7c427f1457a415c29
Alexis Guzman <codingcodax@gmail.com>
2023-03-31 20:51:22 -0600

    feat(tailwind): add font size tokens to theme

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -1,11 +1,13 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 const colors = require('./colors');
+const fontSize = require('./fontSize');
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     colors,
+    fontSize,
     extend: {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans]
