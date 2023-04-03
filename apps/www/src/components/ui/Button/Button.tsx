9154619cc2da91b69ccc29bd6c2f479a4ea96adc
Alexis Guzman <codingcodax@gmail.com>
2023-04-03 10:33:34 -0600

    feat(button): setup primary intent variant

--- apps/www/src/components/ui/Button/Button.tsx
+++ apps/www/src/components/ui/Button/Button.tsx
@@ -5,18 +5,33 @@ import { cn } from '~/utils';
 
 const buttonStyles = tv({
   base: cn(
-    'flex w-fit items-center justify-center font-medium',
-    'focus:outline outline-primary-solid',
+    'flex w-fit items-center justify-center font-semibold',
+    'focus:outline focus:outline-primaryFocusRing focus:outline-2',
   ),
   variants: {
-    intent: {},
-    size: {},
+    intent: {
+      primary: cn(
+        'bg-primarySolid rounded-lg text-mauveBase trasition-colors duration-200',
+        'hover:bg-primarySolidHover',
+        'active:bg-primaryText',
+        'disabled:bg-primaryLine disabled:cursor-not-allowed',
+      ),
+    },
+    size: {
+      small: 'px-3 h-9 text-sm',
+      medium: 'px-4 h-10 text-sm',
+      large: 'px-6 h-12 text-md',
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
   },
 });
 
 type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
 type ButtonVariants = VariantProps<typeof buttonStyles>;
-interface Props extends ButtonProps, ButtonVariants {}
+interface Props extends ButtonProps, ButtonVariants { }
 
 const Button = forwardRef<HTMLButtonElement, Props>(
   ({ intent, size, className, ...props }, ref) => {
