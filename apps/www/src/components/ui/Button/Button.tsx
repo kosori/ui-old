c22c0a4cae62e430481e4a6655e011d4d6696b1a
Alexis Guzman <codingcodax@gmail.com>
2023-04-02 20:18:24 -0600

    feat(ui): setup button with basic styles

--- apps/www/src/components/ui/Button/Button.tsx
+++ apps/www/src/components/ui/Button/Button.tsx
@@ -1,5 +1,36 @@
-const Button = () => {
-  return <div>Button</div>;
-};
+import { forwardRef } from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
 
-export { Button };
+import { cn } from '~/utils';
+
+const buttonStyles = tv({
+  base: cn(
+    'flex w-fit items-center justify-center font-medium',
+    'focus:outline outline-primary-solid',
+  ),
+  variants: {
+    intent: {},
+    size: {},
+  },
+});
+
+type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
+type ButtonVariants = VariantProps<typeof buttonStyles>;
+interface Props extends ButtonProps, ButtonVariants {}
+
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ intent, size, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={buttonStyles({ intent, size, class: className })}
+        {...props}
+      />
+    );
+  },
+);
+
+Button.displayName = 'Button';
+
+export { Button, buttonStyles };
+export type { Props };
