ebc226ec7689cdd520806d75f29a3474a000633a
Alexis Guzman <codingcodax@gmail.com>
2023-04-04 14:14:47 -0600

    feat(components): setup site footer

--- apps/www/src/components/SiteFooter/SiteFooter.tsx
+++ apps/www/src/components/SiteFooter/SiteFooter.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link';
+
 const SiteFooter = () => {
   return (
-    <div>
-      <p>SiteFooter</p>
-    </div>
+    <footer className='flex items-center h-12'>
+      <div className='mx-auto w-full max-w-screen-xl text-center'>
+        <p className='text-sm text-mauveText'>
+          Created by{' '}
+          <Link
+            className='text-primarySolid transition-colors duration-200 hover:text-primarySolidHover'
+            href='/'
+          >
+            Alexis Guzman
+          </Link>
+        </p>
+      </div>
+    </footer>
   );
 };
 
