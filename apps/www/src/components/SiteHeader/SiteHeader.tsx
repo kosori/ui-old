0ae524c3990f3e09349e63496db59599c9212c86
Alexis Guzman <codingcodax@gmail.com>
2023-04-04 17:05:05 -0600

    feat(components): setup site header

--- apps/www/src/components/SiteHeader/SiteHeader.tsx
+++ apps/www/src/components/SiteHeader/SiteHeader.tsx
@@ -1,8 +1,34 @@
+import Link from 'next/link';
+import { SiGithub, SiTwitter } from '@icons-pack/react-simple-icons';
+
+import MainNav from './MainNav';
+
 const SiteHeader = () => {
   return (
-    <div>
-      <p>SiteHeader</p>
-    </div>
+    <header className='flex h-16 w-full items-center'>
+      <div className='mx-auto flex w-full max-w-screen-xl justify-between'>
+        <Link className='font-bold' href='/'>
+          /ui
+        </Link>
+
+        <MainNav />
+
+        <div className='flex gap-x-6'>
+          <Link
+            href='/'
+            className='text-mauveText transition-colors duration-200 hover:text-mauveTextContrast'
+          >
+            <SiGithub size={20} />
+          </Link>
+          <Link
+            className='text-mauveText transition-colors duration-200 hover:text-mauveTextContrast'
+            href='/'
+          >
+            <SiTwitter size={20} />
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
