735de7c4a6b12a22e7748d6e14ce87f30a073525
Alexis Guzman <codingcodax@gmail.com>
2023-04-11 16:31:28 -0600

    feat(accordion): setup root

--- apps/www/src/components/ui/Accordion/Accordion.tsx
+++ apps/www/src/components/ui/Accordion/Accordion.tsx
@@ -1,9 +1,5 @@
-const Accordion = () => {
-  return (
-    <div>
-      <p>Accordion</p>
-    </div>
-  );
-};
+import { Root } from '@radix-ui/react-accordion';
+
+const Accordion = Root;
 
 export default Accordion;
