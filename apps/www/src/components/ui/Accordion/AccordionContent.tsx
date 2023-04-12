a389fef855dde419395a31d8b5f506aeed53853c
Alexis Guzman <codingcodax@gmail.com>
2023-04-12 10:02:42 -0600

    feat(accordion): setup content

--- apps/www/src/components/ui/Accordion/AccordionContent.tsx
+++ apps/www/src/components/ui/Accordion/AccordionContent.tsx
@@ -1,9 +1,19 @@
-const AccordionContent = () => {
-  return (
-    <div>
-      <p>AccordionContent</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { Content } from '@radix-ui/react-accordion';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const AccordionContent = forwardRef<Ref, Props>(
+  ({ className, children, ...props }, ref) => (
+    <Content ref={ref} className={cn('text-mauveText', className)} {...props}>
+      <div className='px-4 py-2'>{children}</div>
+    </Content>
+  ),
+);
+
+AccordionContent.displayName = 'AccordionContent';
 
 export default AccordionContent;
