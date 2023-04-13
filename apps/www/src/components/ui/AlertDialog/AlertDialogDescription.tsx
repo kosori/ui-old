c11eb75b061a657558113072adf932de64d2f013
Alexis Guzman <codingcodax@gmail.com>
2023-04-13 11:22:18 -0600

    feat(dialog): setup description

---
+++ apps/www/src/components/ui/AlertDialog/AlertDialogDescription.tsx
@@ -0,0 +1,21 @@
+import { forwardRef } from 'react';
+import { Description } from '@radix-ui/react-alert-dialog';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Description>;
+type Props = React.ComponentPropsWithoutRef<typeof Description>;
+
+const AlertDialogDescription = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <Description
+      ref={ref}
+      className={cn('text-sm text-mauveText', className)}
+      {...props}
+    />
+  ),
+);
+
+AlertDialogDescription.displayName = Description.displayName;
+
+export default AlertDialogDescription;
