cb69d10581ff814e0b9e96c5429ac9bcbfa87515
Alexis Guzman <codingcodax@gmail.com>
2023-04-17 11:01:48 -0600

    feat(collpsible): export collapsible components

--- apps/www/src/components/ui/Collapsible/index.ts
+++ apps/www/src/components/ui/Collapsible/index.ts
@@ -1,1 +1,3 @@
-export { default } from './Collapsible';
+export { default as Collapsible } from './Collapsible';
+export { default as CollapsibleTrigger } from './CollapsibleTrigger';
+export { default as CollapsibleContent } from './CollapsibleContent';
