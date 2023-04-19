c8f6c8c2a26fcbd08ad82d3bb42b1594be76abc7
Alexis Guzman <codingcodax@gmail.com>
2023-04-19 13:59:15 -0600

    feast(contextmenu): setup sub content

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuSubContent.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react';
+import { SubContent } from '@radix-ui/react-context-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof SubContent>;
+type Props = React.ComponentPropsWithoutRef<typeof SubContent>;
+
+const ContextMenuSubContent = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <SubContent
+      ref={ref}
+      className={cn(
+        'z-50 min-w-[8rem] overflow-hidden rounded-lg border border-mauveLine bg-mauveBase p-1 shadow-md',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+ContextMenuSubContent.displayName = SubContent.displayName;
+
+export default ContextMenuSubContent;
