e8f852385a939e9d23d0875720f308fb600f17b6
Alexis Guzman <codingcodax@gmail.com>
2023-04-19 13:59:46 -0600

    feat(contextmenu): export context menu component

--- apps/www/src/components/ui/ContextMenu/index.ts
+++ apps/www/src/components/ui/ContextMenu/index.ts
@@ -1,1 +1,15 @@
-export { default } from './ContextMenu';
+export { default as ContextMenu } from './ContextMenu';
+export { default as ContextMenuTrigger } from './ContextMenuTrigger';
+export { default as ContextMenuPortal } from './ContextMenuPortal';
+export { default as ContextMenuContent } from './ContextMenuContent';
+export { default as ContextMenuItem } from './ContextMenuItem';
+export { default as ContextMenuGroup } from './ContextMenuGroup';
+export { default as ContextMenuLabel } from './ContextMenuLabel';
+export { default as ContextMenuCheckboxItem } from './ContextMenuCheckboxItem';
+export { default as ContextMenuRadioGroup } from './ContextMenuRadioGroup';
+export { default as ContextMenuRadioItem } from './ContextMenuRadioItem';
+export { default as ContextMenuShortcut } from './ContextMenuShortcut';
+export { default as ContextMenuSeparator } from './ContextMenuSeparator';
+export { default as ContextMenuSub } from './ContextMenuSub';
+export { default as ContextMenuSubTrigger } from './ContextMenuSubTrigger';
+export { default as ContextMenuSubContent } from './ContextMenuSubContent';
