72e9c38933a3388ccabb1343fa7d706360b2fd1e
Alexis Guzman <codingcodax@gmail.com>
2023-04-19 13:56:53 -0600

    feat(contextmenu): setup content

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuContent.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from 'react';
+import { Content, Portal } from '@radix-ui/react-context-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const ContextMenuContent = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <Portal>
+      <Content
+        ref={ref}
+        className={cn(
+          'z-50 min-w-[8rem] overflow-hidden rounded-lg border border-mauveLine bg-mauveBase p-1 shadow-md',
+          className,
+        )}
+        {...props}
+      />
+    </Portal>
+  ),
+);
+
+ContextMenuContent.displayName = Content.displayName;
+
+export default ContextMenuContent;
