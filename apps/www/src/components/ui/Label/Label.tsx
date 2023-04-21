b7c89a2918d6502edaea3a61ae8241fb2b8ae7c7
Alexis Guzman <codingcodax@gmail.com>
2023-04-21 16:07:44 -0600

    feat(ui): setup label

--- apps/www/src/components/ui/Label/Label.tsx
+++ apps/www/src/components/ui/Label/Label.tsx
@@ -1,9 +1,23 @@
-const Label = () => {
-  return (
-    <div>
-      <p>Label</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { Root } from '@radix-ui/react-label';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Root>;
+type Props = React.ComponentPropsWithoutRef<typeof Root>;
+
+const Label = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <Root
+    ref={ref}
+    className={cn(
+      'text-sm font-medium',
+      'peer-disabled:cursor-not-allowed peer-disabled:text-mauveSolid',
+      className,
+    )}
+    {...props}
+  />
+));
+
+Label.displayName = Label.displayName;
 
 export default Label;
