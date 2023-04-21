1e3fffa8c6a675ad62711fff70c96aa3aae02721
Alexis Guzman <codingcodax@gmail.com>
2023-04-21 16:07:37 -0600

    feat(checkbox): change check icon color if disabled

--- apps/www/src/components/ui/Checkbox/Checkbox.tsx
+++ apps/www/src/components/ui/Checkbox/Checkbox.tsx
@@ -11,18 +11,20 @@ const Checkbox = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
   <Root
     ref={ref}
     className={cn(
-      'peer h-4 w-4 rounded-[4px] border border-mauveBorder bg-mauveBase transition-colors duration-200',
+      'group peer h-4 w-4 rounded-[4px] border border-mauveBorder bg-mauveBase transition-colors duration-200',
       'hover:border-mauveBorderHover',
       'focus:outline focus:outline-primaryFocusRing',
       'disabled:cursor-not-allowed disabled:border-mauveLine disabled:bg-mauveBg',
       'data-[state=checked]:border-primaryBorder data-[state=checked]:bg-primaryBg',
       'data-[state=checked]:hover:border-primaryBorderHover',
+      'data-[state=checked]:disabled:border-mauveLine data-[state=checked]:disabled:bg-mauveBg',
+      'data-[state=checked]:disabled:hover:border-mauveLine',
       className,
     )}
     {...props}
   >
     <Indicator className={cn('flex items-center justify-center')}>
-      <Check className='h-4 w-4 text-primarySolid' />
+      <Check className='h-4 w-4 text-primarySolid group-disabled:text-mauveSolid' />
     </Indicator>
   </Root>
 ));
