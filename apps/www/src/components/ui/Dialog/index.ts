1611f554e82b1b2999ee77577ce45a229bcd5b16
Alexis Guzman <codingcodax@gmail.com>
2023-04-26 14:03:17 -0600

    feat(dialog): export dialog components

--- apps/www/src/components/ui/Dialog/index.ts
+++ apps/www/src/components/ui/Dialog/index.ts
@@ -1,1 +1,7 @@
-export { default } from './Dialog';
+export { default as Dialog } from './Dialog';
+export { default as DialogTrigger } from './DialogTrigger';
+export { default as DialogContent } from './DialogContent';
+export { default as DialogTitle } from './DialogTitle';
+export { default as DialogDescription } from './DialogDescription';
+export { default as DialogHeader } from './DialogHeader';
+export { default as DialogFooter } from './DialogFooter';
