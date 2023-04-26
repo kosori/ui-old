f33a2e4ca6b1d3234bde24b18e65c6a567c8b6aa
Alexis Guzman <codingcodax@gmail.com>
2023-04-26 14:02:21 -0600

    feat(dialog): setup content

---
+++ apps/www/src/components/ui/Dialog/DialogContent.tsx
@@ -0,0 +1,45 @@
+import { forwardRef } from 'react';
+import { Close, Content } from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
+
+import { cn } from '~/utils';
+import DialogOverlay from './DialogOverlay';
+import DialogPortal from './DialogPortal';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const DialogContent = forwardRef<Ref, Props>(
+  ({ className, children, ...props }, ref) => (
+    <DialogPortal>
+      <DialogOverlay />
+
+      <Content
+        ref={ref}
+        className={cn(
+          'fixed z-50 grid w-full gap-8 border border-mauveLine bg-mauveBase p-6 shadow-lg',
+          'sm:max-w-lg sm:rounded-lg',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+
+        <Close
+          className={cn(
+            'absolute right-4 top-4 rounded ring-offset-mauveBg transition-opacity',
+            'focus:outline focus:outline-primaryFocusRing',
+            'disabled:cursor-not-allowed disabled:text-mauveText',
+          )}
+        >
+          <X className='h-4 w-4' />
+          <span className='sr-only'>Close</span>
+        </Close>
+      </Content>
+    </DialogPortal>
+  ),
+);
+
+DialogContent.displayName = Content.displayName;
+
+export default DialogContent;
