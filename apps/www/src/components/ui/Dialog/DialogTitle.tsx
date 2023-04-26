d483cfaedde5ab1442f0ad75b344ff049b3d6aef
Alexis Guzman <codingcodax@gmail.com>
2023-04-26 14:03:00 -0600

    feat(dialog): setup title\

---
+++ apps/www/src/components/ui/Dialog/DialogTitle.tsx
@@ -0,0 +1,19 @@
+import { forwardRef } from 'react';
+import { Title } from '@radix-ui/react-dialog';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Title>;
+type Props = React.ComponentPropsWithoutRef<typeof Title>;
+
+const DialogTitle = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <Title
+    ref={ref}
+    className={cn('text-lg font-semibold', className)}
+    {...props}
+  />
+));
+
+DialogTitle.displayName = Title.displayName;
+
+export default DialogTitle;
