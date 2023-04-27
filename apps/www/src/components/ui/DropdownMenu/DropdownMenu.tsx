f7a34b2f9f456fdd4dd1a89bdf71a173e47ed08e
Alexis Guzman <codingcodax@gmail.com>
2023-04-27 10:42:36 -0600

    feat(dropdown): setup root

--- apps/www/src/components/ui/DropdownMenu/DropdownMenu.tsx
+++ apps/www/src/components/ui/DropdownMenu/DropdownMenu.tsx
@@ -1,9 +1,5 @@
-const DropdownMenu = () => {
-  return (
-    <div>
-      <p>DropdownMenu</p>
-    </div>
-  );
-};
+import { Root } from '@radix-ui/react-dropdown-menu';
+
+const DropdownMenu = Root;
 
 export default DropdownMenu;
