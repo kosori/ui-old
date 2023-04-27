2e0620edf900dd88fc5808a1555be1e6aca3f197
Alexis Guzman <codingcodax@gmail.com>
2023-04-27 10:46:15 -0600

    feat(dropdown): export dropdown menu components

--- apps/www/src/components/ui/DropdownMenu/index.ts
+++ apps/www/src/components/ui/DropdownMenu/index.ts
@@ -1,1 +1,15 @@
-export { default } from './DropdownMenu';
+export { default as DropdownMenu } from './DropdownMenu';
+export { default as DropdownMenuTrigger } from './DropdownMenuTrigger';
+export { default as DropdownMenuPortal } from './DropdownMenuPortal';
+export { default as DropdownMenuContent } from './DropdownMenuContent';
+export { default as DropdownMenuItem } from './DropdownMenuItem';
+export { default as DropdownMenuGroup } from './DropdownMenuGroup';
+export { default as DropdownMenuLabel } from './DropdownMenuLabel';
+export { default as DropdownMenuCheckboxItem } from './DropdownMenuCheckboxItem';
+export { default as DropdownMenuRadioGroup } from './DropdownMenuRadioGroup';
+export { default as DropdownMenuRadioItem } from './DropdownMenuRadioItem';
+export { default as DropdownMenuSeparator } from './DropdownMenuSeparator';
+export { default as DropdownMenuShortcut } from './DropdownMenuShortcut';
+export { default as DropdownMenuSub } from './DropdownMenuSub';
+export { default as DropdownMenuSubTrigger } from './DropdownMenuSubTrigger';
+export { default as DropdownMenuSubContent } from './DropdownMenuSubContent';
