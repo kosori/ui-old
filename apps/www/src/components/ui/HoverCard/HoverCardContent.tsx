a634f2c8d55371a4f6d94003e3ad2fe45a27993f
Alexis Guzman <codingcodax@gmail.com>
2023-04-28 11:01:25 -0600

    feat(hovercard): setup content

---
+++ apps/www/src/components/ui/HoverCard/HoverCardContent.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from 'react';
+import { Content } from '@radix-ui/react-hover-card';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const HoverCardContent = forwardRef<Ref, Props>(
+  ({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+    <Content
+      ref={ref}
+      align={align}
+      className={cn(
+        'z-50 rounded-lg bg-mauveBase p-4 shadow-md outline-none',
+        className,
+      )}
+      sideOffset={sideOffset}
+      {...props}
+    />
+  ),
+);
+
+HoverCardContent.displayName = Content.displayName;
+
+export default HoverCardContent;
