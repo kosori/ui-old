c0e1933ec0c18ea11c0ead8db321a5e7ed1980f7
Alexis Guzman <codingcodax@gmail.com>
2023-05-02 17:12:08 -0600

    feat(menubar): setup content

---
+++ apps/www/src/components/ui/Menubar/MenubarContent.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react';
+import { Content, Portal } from '@radix-ui/react-menubar';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const MenubarContent = forwardRef<Ref, Props>(
+  (
+    { className, align = 'start', alignOffset = -4, sideOffset = 8, ...props },
+    ref,
+  ) => (
+    <Portal>
+      <Content
+        ref={ref}
+        align={align}
+        alignOffset={alignOffset}
+        className={cn(
+          'z-50 min-w-[12rem] overflow-hidden rounded-lg border border-mauveLine bg-mauveBase p-1 shadow-md',
+          className,
+        )}
+        sideOffset={sideOffset}
+        {...props}
+      />
+    </Portal>
+  ),
+);
+
+MenubarContent.displayName = Content.displayName;
+
+export default MenubarContent;
