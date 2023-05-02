99f8c90401a62597fdcacbf529199ed0e8cb1fbc
Alexis Guzman <codingcodax@gmail.com>
2023-05-02 17:13:32 -0600

    feat(menubar): setup sub content

---
+++ apps/www/src/components/ui/Menubar/MenubarSubContent.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react';
+import { SubContent } from '@radix-ui/react-menubar';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof SubContent>;
+type Props = React.ComponentPropsWithoutRef<typeof SubContent>;
+
+const MenubarSubContent = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <SubContent
+      ref={ref}
+      className={cn(
+        'z-50 min-w-[8rem] overflow-hidden rounded-lg border border-mauveLine bg-mauveBase p-1 shadow-md',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+MenubarSubContent.displayName = SubContent.displayName;
+
+export default MenubarSubContent;
