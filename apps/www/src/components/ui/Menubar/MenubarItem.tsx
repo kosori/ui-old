0e583a959196afd1dfad513cbbe0ecf64a70f79e
Alexis Guzman <codingcodax@gmail.com>
2023-05-02 17:12:22 -0600

    feat(menubar): setup item

---
+++ apps/www/src/components/ui/Menubar/MenubarItem.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import { Item } from '@radix-ui/react-menubar';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Item>;
+type Props = React.ComponentPropsWithoutRef<typeof Item> & { inset?: boolean };
+
+const MenubarItem = forwardRef<Ref, Props>(
+  ({ className, inset, ...props }, ref) => (
+    <Item
+      ref={ref}
+      className={cn(
+        'group relative flex h-8 select-none items-center rounded-md px-2 text-sm outline-none transition-colors duration-200',
+        'focus:bg-primaryBgHover',
+        'active:bg-primaryBgActive',
+        'data-[disabled]:cursor-not-allowed data-[disabled]:text-mauveSolid',
+        inset && 'pl-8',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+MenubarItem.displayName = Item.displayName;
+
+export default MenubarItem;
