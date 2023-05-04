11277f5687b291f2d6df288d0862532db63e97f4
Alexis Guzman <codingcodax@gmail.com>
2023-05-04 14:25:34 -0600

    feat(navmenu): export navigation menu components

--- apps/www/src/components/ui/NavigationMenu/index.ts
+++ apps/www/src/components/ui/NavigationMenu/index.ts
@@ -1,1 +1,11 @@
-export { default } from './NavigationMenu';
+export { default as NavigationMenu } from './NavigationMenu';
+export { default as NavigationMenuList } from './NavigationMenuList';
+export { default as NavigationMenuItem } from './NavigationMenuItem';
+export {
+  default as NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from './NavigationMenuTrigger';
+export { default as NavigationMenuContent } from './NavigationMenuContent';
+export { default as NavigationMenuLink } from './NavigationMenuLink';
+export { default as NavigationMenuViewport } from './NavigationMenuViewport';
+export { default as NavigationMenuIndicator } from './NavigationMenuIndicator';
