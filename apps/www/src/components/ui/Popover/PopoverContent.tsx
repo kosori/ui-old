b0c234a253036f5a8fc436960007cde974c4bc63
Alexis Guzman <codingcodax@gmail.com>
2023-05-06 09:38:58 -0600

    feat(popover): setup content

---
+++ apps/www/src/components/ui/Popover/PopoverContent.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import { Content, Portal } from '@radix-ui/react-popover';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const PopoverContent = forwardRef<Ref, Props>(
+  ({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+    <Portal>
+      <Content
+        ref={ref}
+        align={align}
+        className={cn(
+          'z-50 w-72 rounded-lg border border-mauveLine bg-mauveBase p-4 shadow-md outline-none',
+          className,
+        )}
+        sideOffset={sideOffset}
+        {...props}
+      />
+    </Portal>
+  ),
+);
+
+PopoverContent.displayName = Content.displayName;
+
+export default PopoverContent;
