356d25ff6f9df09c599441895a8adb76dd17b93d
Alexis Guzman <codingcodax@gmail.com>
2023-05-08 10:34:08 -0600

    feat(progress): setup root

---
+++ apps/www/src/components/ui/Progress/Progress.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from 'react';
+import { Indicator, Root } from '@radix-ui/react-progress';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Root>;
+type Props = React.ComponentPropsWithoutRef<typeof Root>;
+
+const Progress = forwardRef<Ref, Props>(
+  ({ className, value, ...props }, ref) => (
+    <Root
+      ref={ref}
+      className={cn(
+        'relative h-4 w-full overflow-hidden rounded-full bg-mauveBgSubtle',
+        className,
+      )}
+      {...props}
+    >
+      <Indicator
+        className='h-full w-full flex-1 bg-primarySolid transition-all'
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </Root>
+  ),
+);
+
+Progress.displayName = Root.displayName;
+
+export default Progress;
