e6e13e607c0d39a0763f80970723c56749168394
Alexis Guzman <codingcodax@gmail.com>
2023-05-09 12:06:05 -0600

    feat(radio): setup root

--- apps/www/src/components/ui/RadioGroup/RadioGroup.tsx
+++ apps/www/src/components/ui/RadioGroup/RadioGroup.tsx
@@ -1,9 +1,15 @@
-const RadioGroup = () => {
-  return (
-    <div>
-      <p>RadioGroup</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { Root } from '@radix-ui/react-radio-group';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Root>;
+type Props = React.ComponentPropsWithoutRef<typeof Root>;
+
+const RadioGroup = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <Root ref={ref} className={cn('grid gap-2', className)} {...props} />
+));
+
+RadioGroup.displayName = Root.displayName;
 
 export default RadioGroup;
