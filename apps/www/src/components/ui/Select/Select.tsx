4d8ac0b436fabe383ec864d16e7ae95a5dca4a76
Alexis Guzman <codingcodax@gmail.com>
2023-05-11 17:47:56 -0600

    feat(select): setup root

--- apps/www/src/components/ui/Select/Select.tsx
+++ apps/www/src/components/ui/Select/Select.tsx
@@ -1,9 +1,5 @@
-const Select = () => {
-  return (
-    <div>
-      <p>Select</p>
-    </div>
-  );
-};
+import { Root } from '@radix-ui/react-select';
+
+const Select = Root;
 
 export default Select;
