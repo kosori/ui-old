c85860485362fd55ce25f2883b756c139053f34f
Alexis Guzman <codingcodax@gmail.com>
2023-05-11 17:49:03 -0600

    feat(select): export select components

--- apps/www/src/components/ui/Select/index.ts
+++ apps/www/src/components/ui/Select/index.ts
@@ -1,1 +1,7 @@
-export { default } from './Select';
+export { default as Select } from './Select';
+export { default as SelectTrigger } from './SelectTrigger';
+export { default as SelectValue } from './SelectValue';
+export { default as SelectContent } from './SelectContent';
+export { default as SelectGroup } from './SelectGroup';
+export { default as SelectLabel } from './SelectLabel';
+export { default as SelectItem } from './SelectItem';
