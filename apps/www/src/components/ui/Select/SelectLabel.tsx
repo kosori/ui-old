3921fc133f4843fcab70a07c0b7354503ab969a8
Alexis Guzman <codingcodax@gmail.com>
2023-05-11 17:48:26 -0600

    feat(select): setup label

---
+++ apps/www/src/components/ui/Select/SelectLabel.tsx
@@ -0,0 +1,22 @@
+import { forwardRef } from 'react';
+import { Label } from '@radix-ui/react-select';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Label>;
+type Props = React.ComponentPropsWithoutRef<typeof Label>;
+
+const SelectLabel = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <Label
+    ref={ref}
+    className={cn(
+      'flex h-8 items-center pl-8 pr-2 text-xs font-medium text-mauveText',
+      className,
+    )}
+    {...props}
+  />
+));
+
+SelectLabel.displayName = Label.displayName;
+
+export default SelectLabel;
