dfdf3fd635df7795a04ba156ed003c19ea23d1fd
Alexis Guzman <codingcodax@gmail.com>
2023-05-22 12:58:51 -0600

    feat(checkbox): add size and shape variants

--- apps/www/src/components/ui/Checkbox/Checkbox.tsx
+++ apps/www/src/components/ui/Checkbox/Checkbox.tsx
@@ -1,32 +1,54 @@
 import { forwardRef } from 'react';
 import { Indicator, Root } from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
+import { tv, type VariantProps } from 'tailwind-variants';
 
 import { cn } from '~/utils';
 
-type Ref = React.ElementRef<typeof Root>;
-type Props = React.ComponentPropsWithoutRef<typeof Root>;
+const checkboxStyles = tv({
+  base: cn(
+    'group peer border border-mauveBorder bg-mauveBase outline-none transition-colors duration-200',
+    'hover:border-mauveBorderHover',
+    'focus-visible:ring-4 focus-visible:ring-mauveFocusRing',
+    'disabled:cursor-not-allowed disabled:border-0 disabled:bg-mauveLine',
+    'data-[state=checked]:border-0 data-[state=checked]:bg-primarySolid',
+    'data-[state=checked]:focus-visible:ring-primaryFocusRing',
+    'data-[state=checked]:disabled:border-0 data-[state=checked]:disabled:bg-mauveLine',
+  ),
+  variants: {
+    shape: {
+      square: 'rounded-md',
+      round: 'rounded-full',
+    },
+    size: {
+      small: 'h-4 w-4 p-[2px]',
+      medium: 'h-[18px] w-[18px] p-[2px]',
+    },
+  },
+  defaultVariants: {
+    shape: 'square',
+    size: 'small',
+  },
+});
 
-const Checkbox = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
-  <Root
-    ref={ref}
-    className={cn(
-      'group peer h-5 w-5 rounded-md border border-mauveBorder bg-mauveBase outline-none transition-colors duration-200',
-      'hover:border-mauveBorderHover',
-      'focus-visible:ring-mauveFocusRing focus-visible:ring-4',
-      'disabled:cursor-not-allowed disabled:border-0 disabled:bg-mauveLine',
-      'data-[state=checked]:border-0 data-[state=checked]:bg-primarySolid',
-      'data-[state=checked]:focus-visible:ring-primaryFocusRing',
-      'data-[state=checked]:disabled:border-0 data-[state=checked]:disabled:bg-mauveLine',
-      className,
-    )}
-    {...props}
-  >
-    <Indicator className={cn('flex items-center justify-center')}>
-      <Check className='h-4 w-4 text-mauveBase group-disabled:text-mauveSolid' />
-    </Indicator>
-  </Root>
-));
+type Ref = React.ElementRef<typeof Root>;
+type CheckboxProps = React.ComponentPropsWithoutRef<typeof Root>;
+type CheckboxVariants = VariantProps<typeof checkboxStyles>;
+interface Props extends CheckboxProps, CheckboxVariants { }
+
+const Checkbox = forwardRef<Ref, Props>(
+  ({ shape, size, className, ...props }, ref) => (
+    <Root
+      ref={ref}
+      className={checkboxStyles({ shape, size, class: className })}
+      {...props}
+    >
+      <Indicator className={cn('flex items-center justify-center')}>
+        <Check className='h-full w-full text-mauveBase group-disabled:text-mauveSolid' />
+      </Indicator>
+    </Root>
+  ),
+);
 
 Checkbox.displayName = Root.displayName;
 
