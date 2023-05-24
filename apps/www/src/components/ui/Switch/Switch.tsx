5b0923b91aff6a0eee6767f1c64df0d5eab9827e
Alexis Guzman <codingcodax@gmail.com>
2023-05-23 20:56:16 -0600

    feat(switch): create small and medium sizes

--- apps/www/src/components/ui/Switch/Switch.tsx
+++ apps/www/src/components/ui/Switch/Switch.tsx
@@ -1,28 +1,53 @@
 import { forwardRef } from 'react';
 import { Root, Thumb } from '@radix-ui/react-switch';
+import { tv, type VariantProps } from 'tailwind-variants';
 
 import { cn } from '~/utils';
 
-type Ref = React.ElementRef<typeof Root>;
-type Props = React.ComponentPropsWithoutRef<typeof Root>;
+const switchStyles = tv({
+  base: cn(
+    'peer inline-flex cursor-pointer items-center rounded-full border-2 border-transparent outline-none transition-colors',
+    'focus-visible:ring-4',
+    'disabled:cursor-not-allowed disabled:bg-mauveBgSubtle',
+    'data-[state=unchecked]:bg-mauveLine',
+    'data-[state=checked]:bg-primarySolid',
+    'data-[state=unchecked]:focus-visible:ring-mauveFocusRing',
+    'data-[state=checked]:focus-visible:ring-primaryFocusRing',
+  ),
+  variants: {
+    size: {
+      small: cn(
+        'h-4 w-7',
+        '[&_span]:h-3 [&_span]:w-3',
+        '[&_span]:data-[state=checked]:translate-x-3',
+      ),
+      medium: cn(
+        'h-5 w-[34px]',
+        '[&_span]:h-4 [&_span]:w-4',
+        '[&_span]:data-[state=checked]:translate-x-3.5',
+      ),
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
+});
 
-const Switch = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+type Ref = React.ElementRef<typeof Root>;
+type SwitchProps = React.ComponentPropsWithoutRef<typeof Root>;
+type SwitchVariants = VariantProps<typeof switchStyles>;
+interface Props extends SwitchProps, SwitchVariants {}
+
+const Switch = forwardRef<Ref, Props>(({ size, className, ...props }, ref) => (
   <Root
     ref={ref}
-    className={cn(
-      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors',
-      'focus-visible:outline focus-visible:outline-primaryFocusRing',
-      'disabled:cursor-not-allowed disabled:bg-mauveBgSubtle',
-      'data-[state=checked]:bg-primarySolid',
-      'data-[state=unchecked]:bg-mauveBg',
-      className,
-    )}
+    className={switchStyles({ size, class: className })}
     {...props}
   >
     <Thumb
       className={cn(
-        'pointer-events-none block h-5 w-5 rounded-full bg-mauveBase shadow-lg transition-transform',
-        'data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0',
+        'pointer-events-none block rounded-full bg-mauveBase shadow-lg transition-transform',
+        'data-[state=unchecked]:translate-x-0',
       )}
     />
   </Root>
