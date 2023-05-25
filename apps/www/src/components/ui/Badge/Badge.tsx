decaa2f9c8071c3fbd8897611a759ce4478409b5
Alexis Guzman <codingcodax@gmail.com>
2023-05-25 16:47:20 -0600

    feat(badge): setup component

--- apps/www/src/components/ui/Badge/Badge.tsx
+++ apps/www/src/components/ui/Badge/Badge.tsx
@@ -1,9 +1,40 @@
-const Badge = () => {
-  return (
-    <div>
-      <p>Badge</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+import { cn } from '~/utils';
+
+const badgeStyles = tv({
+  base: cn(
+    'inline-flex select-none text-mauveBase items-center rounded-full px-2.5 py-1 text-xs font-semibold',
+  ),
+  variants: {
+    intent: {
+      default: 'bg-mauveTextContrast',
+      info: 'bg-blue9',
+      success: 'bg-successSolid',
+      warning: 'bg-warningSolid',
+      danger: 'bg-dangerSolid',
+    },
+  },
+  defaultVariants: {
+    intent: 'default',
+  },
+});
+
+type BadgeProps = React.ComponentPropsWithoutRef<'div'>;
+type BadgeVariants = VariantProps<typeof badgeStyles>;
+interface Props extends BadgeProps, BadgeVariants { }
+
+const Badge = forwardRef<HTMLDivElement, Props>(
+  ({ intent, className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={badgeStyles({ intent, class: className })}
+      {...props}
+    />
+  ),
+);
+
+Badge.displayName = 'Badge';
 
 export default Badge;
