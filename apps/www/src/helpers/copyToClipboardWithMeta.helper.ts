01120837b30b66ed01ddfad88cb300c900f22d70
Alexis Guzman <codingcodax@gmail.com>
2023-05-30 10:00:47 -0600

    feat(helpers): create copy to clipboard with meta function

---
+++ apps/www/src/helpers/copyToClipboardWithMeta.helper.ts
@@ -0,0 +1,10 @@
+import type { Event } from '~/types';
+import { trackEvent } from '~/utils';
+
+const copyToClipboardWithMeta = async (value: string, event?: Event) => {
+  await navigator.clipboard.writeText(value);
+
+  if (event) trackEvent(event);
+};
+
+export default copyToClipboardWithMeta;
