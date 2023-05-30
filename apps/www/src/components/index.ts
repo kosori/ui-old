46afe834fc9aa681c306ac6c829659ad87d003e5
Alexis Guzman <codingcodax@gmail.com>
2023-05-30 10:35:57 -0600

    feat(components): export copy npm command button

--- apps/www/src/components/index.ts
+++ apps/www/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CopyButton } from './CopyButton';
+export { default as CopyNpmCommandButton } from './CopyNpmCommandButton';
 export { default as SiteFooter } from './SiteFooter';
 export { default as SiteHeader } from './SiteHeader';
