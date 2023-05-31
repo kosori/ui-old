6c93e3d592e73f2296dd8ee08944954cd3287ab5
Alexis Guzman <codingcodax@gmail.com>
2023-05-31 10:55:40 -0600

    feat(components): setup component source

--- apps/www/src/components/ComponentSource/ComponentSource.tsx
+++ apps/www/src/components/ComponentSource/ComponentSource.tsx
@@ -1,8 +1,20 @@
-const ComponentSource = () => {
+import { type HTMLAttributes } from 'react';
+
+import { CodeBlockWrapper } from '~/components';
+import { cn } from '~/utils';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  src: string;
+}
+
+const ComponentSource = ({ children, className }: Props) => {
   return (
-    <div>
-      <p>ComponentSource</p>
-    </div>
+    <CodeBlockWrapper
+      className={cn('my-6 overflow-hidden rounded-md', className)}
+      expandButtonTitle='Expand'
+    >
+      {children}
+    </CodeBlockWrapper>
   );
 };
 
