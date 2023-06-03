2cff94234cb282b19aa7df2b83f8435eed7748a0
Alexis Guzman <codingcodax@gmail.com>
2023-06-03 16:35:12 -0600

    chore(nextconfig): update import path

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -3,10 +3,11 @@
  * This is especially useful for Docker builds and Linting.
  */
 // !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-import { withContentlayer } from 'next-contentlayer/.';
+import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import("next").NextConfig} */
 const config = {
+  swcMinify: true,
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: [],
   /** We already do linting and typechecking as separate tasks in CI */
