2c1f179d4b026994b5414b032dbc797a8665a386
Alexis Guzman <codingcodax@gmail.com>
2023-06-06 17:41:19 -0600

    feat(config): create docs config file

---
+++ apps/www/src/config/docs.ts
@@ -0,0 +1,41 @@
+const docs = {
+  sidebarNav: [
+    {
+      title: 'Getting Started',
+      items: [
+        {
+          title: 'Introduction',
+          href: '/docs/Introduction',
+          items: [],
+        },
+        {
+          title: 'Installation',
+          href: '/docs/installation',
+          items: [],
+        },
+        {
+          title: 'Theming',
+          href: '/docs/theming',
+          items: [],
+        },
+        // {
+        //   title: 'CLI',
+        //   href: '/docs/cli',
+        //   items: [],
+        // },
+      ],
+    },
+    {
+      title: 'Components',
+      items: [
+        {
+          title: 'Accordion',
+          href: '/docs/accordion',
+          items: [],
+        },
+      ],
+    },
+  ],
+};
+
+export default docs;
