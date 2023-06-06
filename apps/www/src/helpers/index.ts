671befd861526ca13a24416050b93c192553bdfc
Alexis Guzman <codingcodax@gmail.com>
2023-06-05 18:22:59 -0600

    fix(helpers): remove rehype exports

--- apps/www/src/helpers/index.ts
+++ apps/www/src/helpers/index.ts
@@ -1,3 +1,1 @@
 export { default as copyToClipboardWithMeta } from './copyToClipboardWithMeta.helper';
-export { default as rehypeComponent } from './rehypeComponent.helper';
-export { default as rehypeNpmCommand } from './rehypeNpmCommand.helper';
