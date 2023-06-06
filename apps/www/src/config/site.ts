4df530857852c3053842fc6de984aee35ba867e3
Alexis Guzman <codingcodax@gmail.com>
2023-06-06 17:40:54 -0600

    feat(config): create site config file

---
+++ apps/www/src/config/site.ts
@@ -0,0 +1,15 @@
+const url = 'https://zenui.codingcodax.dev';
+
+const site = {
+  name: 'Zen UI',
+  url,
+  ogImage: `${url}/og.jpg`,
+  description:
+    'Discover a world of possibilities with our beautifully designed components built with Radix UI and Tailwind CSS. Experience accessibility, unparalleled customization, and the power of open source.',
+  links: {
+    twitter: 'https://twitter.com/codingcodax',
+    github: 'https://github.com/codingcodax/zen-ui',
+  },
+};
+
+export default site;
