992d45514b95b760469730241b4e1e85a590fd46
Alexis Guzman <codingcodax@gmail.com>
2023-06-07 17:50:24 -0600

    feat(types): create nav item, nav item with children and sidebar nav item

--- apps/www/src/types/nav.d.ts
+++ apps/www/src/types/nav.d.ts
@@ -3,6 +3,7 @@ import type { z } from 'zod';
 import type { navItemSchema } from '~/schemas';
 
 export type NavItem = z.infer<typeof navItemSchema>;
+
 export interface NavItemWithChildren extends NavItem {
   items: NavItemWithChildren[];
 }
