31a3ccb2b2b6ee9e5ac4d9356667a58c6145ff5f
Alexis Guzman <codingcodax@gmail.com>
2023-06-07 17:46:23 -0600

    feat(docs): setup docs sidebar nav items component

--- apps/www/src/components/DocsSidebarNav/DocsSidebarNavItems.tsx
+++ apps/www/src/components/DocsSidebarNav/DocsSidebarNavItems.tsx
@@ -1,7 +1,23 @@
-const DocsSidebarNavItems = () => {
+import type { SidebarNavItem } from '~/types';
+import DocsSidebarNavItem from './DocsSidebarNavItem';
+
+type Props = {
+  items: SidebarNavItem[];
+  pathname: string | null;
+};
+
+const DocsSidebarNavItems = ({ items, pathname }: Props) => {
   return (
-    <div>
-      <p>DocsSidebarNavItems</p>
+    <div className='relative grid grid-flow-row gap-0.5 pr-2'>
+      {items.map((item, index) => (
+        <DocsSidebarNavItem
+          key={`${item}-${index}`}
+          item={item}
+          pathname={pathname}
+        />
+      ))}
+
+      <div className='absolute left-2 top-0 h-full w-px bg-mauveLine' />
     </div>
   );
 };
