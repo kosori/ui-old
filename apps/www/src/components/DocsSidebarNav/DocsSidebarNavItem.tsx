a61f27b84b57dccccd6cf6714014e67363849b42
Alexis Guzman <codingcodax@gmail.com>
2023-06-07 17:46:12 -0600

    feat(docs): setup docs sidebar nav item component

--- apps/www/src/components/DocsSidebarNav/DocsSidebarNavItem.tsx
+++ apps/www/src/components/DocsSidebarNav/DocsSidebarNavItem.tsx
@@ -1,8 +1,69 @@
-const DocsSidebarNavItem = () => {
+import Link from 'next/link';
+
+import type { SidebarNavItem } from '~/types';
+import { cn } from '~/utils';
+
+type Props = {
+  item: SidebarNavItem;
+  pathname: string | null;
+};
+
+const DocsSidebarNavItem = ({ item, pathname }: Props) => {
+  if (!item.href && item.disabled) {
+    return (
+      <span
+        className={cn(
+          'flex w-full cursor-not-allowed items-center rounded-md p-2',
+          item.disabled && 'cursor-not-allowed text-mauveText',
+        )}
+      >
+        {item.title}
+        {item.label && (
+          <span
+            className={cn(
+              'ml-2 rounded-md px-1.5 py-0.5 text-xs no-underline',
+              'group-hover:no-underline',
+            )}
+          >
+            {item.label}
+          </span>
+        )}
+      </span>
+    );
+  }
+
   return (
-    <div>
-      <p>DocsSidebarNavItem</p>
-    </div>
+    <Link
+      className={cn(
+        'relative w-full items-center rounded-md px-5 py-1.5 text-sm transition-colors duration-200',
+        'hover:bg-primaryBgHover',
+        item.disabled && 'cursor-not-allowed text-mauveText',
+        pathname === item.href
+          ? cn(
+              'bg-primaryBgActive text-mauveTextContrast',
+              'hover:bg-primaryBgActive',
+            )
+          : 'text-mauveText',
+      )}
+      href={item.href || '#'}
+      rel={item.external ? 'noopener noreferrer' : ''}
+      target={item.external ? '_blank' : ''}
+    >
+      {item.title}
+      {item.label && (
+        <span
+          className={cn(
+            'rounded-md bg-mauveBg px-1.5 py-0.5 text-mauveTextContrast no-underline',
+            'group-hover:no-underline',
+          )}
+        >
+          {item.label}
+        </span>
+      )}
+      {pathname === item.href && (
+        <div className='absolute left-2 top-1 z-30 h-6 w-px bg-primarySolid' />
+      )}
+    </Link>
   );
 };
 
