0c988ea970ec573f418cf7fac684fe9f4b777a5a
Alexis Guzman <codingcodax@gmail.com>
2023-06-07 17:48:53 -0600

    feat(schemas): create nav item

--- apps/www/src/schemas/nav.ts
+++ apps/www/src/schemas/nav.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
 
-const navItem = z.object({
+export const navItemSchema = z.object({
   title: z.string(),
   href: z.string().optional(),
   disabled: z.boolean().optional(),
   external: z.boolean().optional(),
   label: z.string().optional(),
 });
-
-export default navItem;
