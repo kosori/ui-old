76edb4de927bc9cd66d2c1d3fea98a71ae65a6d8
Alexis Guzman <codingcodax@gmail.com>
2023-06-09 08:49:25 -0600

    feat(types): export toc types

--- apps/www/src/types/index.d.ts
+++ apps/www/src/types/index.d.ts
@@ -1,3 +1,4 @@
 export { default as Event } from './event';
 export { NavItem, NavItemWithChildren, SidebarNavItem } from './nav';
+export { TocItem, TocItems, TableOfContents } from './toc';
 export { NpmCommands, UnistNode, UnistTree } from './unist';
