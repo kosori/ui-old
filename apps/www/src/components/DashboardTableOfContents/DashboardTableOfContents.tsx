29f334eaf1c24888640000a9bf471f502a738bb5
Alexis Guzman <codingcodax@gmail.com>
2023-06-09 12:24:15 -0600

    feat(docs): setup dashboard table of contents

--- apps/www/src/components/DashboardTableOfContents/DashboardTableOfContents.tsx
+++ apps/www/src/components/DashboardTableOfContents/DashboardTableOfContents.tsx
@@ -1,7 +1,35 @@
-const DashboardTableOfContents = () => {
+import { useMemo } from 'react';
+
+import { useActiveItem, useMounted } from '~/hooks';
+import type { TableOfContents } from '~/types';
+import DashboardTableOfContentsTree from './DashboardTableOfContentsTree';
+
+type Props = {
+  toc: TableOfContents;
+};
+
+const DashboardTableOfContents = ({ toc }: Props) => {
+  const itemIds = useMemo(
+    () =>
+      toc?.items
+        ? toc.items
+            .flatMap((item) => [item.url, item?.items?.map((item) => item.url)])
+            .flat()
+            .filter(Boolean)
+            .map((id) => id?.split('#')[1] || '')
+        : [],
+    [toc],
+  );
+
+  const activeHeading = useActiveItem(itemIds);
+  const mounted = useMounted();
+
+  if (!toc?.items || !mounted) return null;
+
   return (
-    <div>
-      <p>DashboardTableOfContents</p>
+    <div className='space-y-2'>
+      <p className='font-medium'>On This Page</p>
+      <DashboardTableOfContentsTree activeItem={activeHeading} tree={toc} />
     </div>
   );
 };
