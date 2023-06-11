2ce3c817ada8c5025a48fad7e26902352adcb947
Alexis Guzman <codingcodax@gmail.com>
2023-06-10 19:37:23 -0600

    feat(breadcrumb): setup separator

--- apps/www/src/components/ui/Breadcrumb/BreadcrumbSeparator.tsx
+++ apps/www/src/components/ui/Breadcrumb/BreadcrumbSeparator.tsx
@@ -1,8 +1,21 @@
-const BreadcrumbSeparator = () => {
+import { ArrowRight, ChevronRight, CircleDot } from 'lucide-react';
+
+import { cn } from '~/utils';
+
+type Props = {
+  type?: 'slash' | 'vertical-bar' | 'chevron' | 'arrow' | 'dot';
+  className?: string;
+};
+
+const BreadcrumbSeparator = ({ type = 'slash', className }: Props) => {
   return (
-    <div>
-      <p>BreadcrumbSeparator</p>
-    </div>
+    <span className={cn('text-mauve11', className)}>
+      {type === 'slash' && '/'}
+      {type === 'vertical-bar' && '|'}
+      {type === 'chevron' && <ChevronRight className='h-4 w-4' />}
+      {type === 'arrow' && <ArrowRight className='h-4 w-4' />}
+      {type === 'dot' && <CircleDot className='h-4 w-4' />}
+    </span>
   );
 };
 
