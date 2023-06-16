05074f19ec39a3d92b3e504a8c57c3090d0c9789
Alexis Guzman <codingcodax@gmail.com>
2023-06-16 15:25:59 -0600

    feat(components): setup site tech stack item

--- apps/www/src/components/SiteTechStack/SiteTechStackItem.tsx
+++ apps/www/src/components/SiteTechStack/SiteTechStackItem.tsx
@@ -1,9 +1,19 @@
-const SiteTechStackItem = () => {
+type Props = {
+  children: React.ReactNode;
+};
+
+export const SiteTechStackItem = ({ children }: Props) => {
   return (
-    <div>
-      <p>SiteTechStackItem</p>
+    <div className='grid content-center justify-items-center gap-y-2 text-center'>
+      {children}
     </div>
   );
 };
 
-export default SiteTechStackItem;
+export const SiteTechStackItemTitle = ({ children }: Props) => {
+  return <h3 className='text-lg font-medium'>{children}</h3>;
+};
+
+export const SiteTechStackItemDescription = ({ children }: Props) => {
+  return <p className='text-md text-mauveText'>{children}</p>;
+};
