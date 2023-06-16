06bb0714e9c99b79a82da173e86ee68f69f72652
Alexis Guzman <codingcodax@gmail.com>
2023-06-16 17:38:06 -0600

    feat(icons): export lucide, radix and tailwind icons

---
+++ apps/www/src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Lucide } from './Lucide';
+export { default as RadixUI } from './RadixUI';
+export { default as TailwindCSS } from './TailwindCSS';
+export { default as TailwindVariants } from './TailwindVariants';
