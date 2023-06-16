ff244e81f1561680634e6a3c08f0b4141eeac55b
Alexis Guzman <codingcodax@gmail.com>
2023-06-16 17:27:03 -0600

    feat(components): setup site features

--- apps/www/src/components/SiteFeatures/SiteFeatures.tsx
+++ apps/www/src/components/SiteFeatures/SiteFeatures.tsx
@@ -1,8 +1,42 @@
+import {
+  SiteFeature,
+  SiteFeatureDescription,
+  SiteFeatureTitle,
+} from './SiteFeature';
+
+const features = [
+  {
+    title: 'Accessible',
+    description:
+      'Components built with Radix UI. Screen reader, RTL and typehead support and keyboard navigation.',
+  },
+  {
+    title: 'Customizable',
+    description:
+      'We utilize CSS variables for styling and tailwind-variants to have different types and sizes of a component.',
+  },
+  {
+    title: 'Open Source',
+    description:
+      'You can actively contribute to this project by sharing ideas and reporting bugs.',
+  },
+  {
+    title: 'Fast',
+    description:
+      'Build applications faster by copying and pasting components, saving valuable development time.',
+  },
+];
+
 const SiteFeatures = () => {
   return (
-    <div>
-      <p>SiteFeatures</p>
-    </div>
+    <section className='mx-auto mt-32 grid w-full max-w-screen-xl grid-flow-col gap-x-6'>
+      {features.map(({ title, description }) => (
+        <SiteFeature key={title}>
+          <SiteFeatureTitle>{title}</SiteFeatureTitle>
+          <SiteFeatureDescription>{description}</SiteFeatureDescription>
+        </SiteFeature>
+      ))}
+    </section>
   );
 };
 
