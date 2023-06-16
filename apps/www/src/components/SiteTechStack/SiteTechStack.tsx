131ff75111071f9fe0ef883a95e89626f5a93446
Alexis Guzman <codingcodax@gmail.com>
2023-06-16 17:36:52 -0600

    feat(components): seutp site tech stack

--- apps/www/src/components/SiteTechStack/SiteTechStack.tsx
+++ apps/www/src/components/SiteTechStack/SiteTechStack.tsx
@@ -1,7 +1,16 @@
+import SiteTechStackItems from './SiteTechStackItems';
+
 const SiteTechStack = () => {
   return (
-    <div>
-      <p>SiteTechStack</p>
+    <div className='mt-52 grid gap-y-10'>
+      <div className='space-y-2 text-center'>
+        <h2 className='text-3xl font-semibold'>Our tech stack</h2>
+        <p className='text-lg text-mauveText'>
+          The tools that make this possible.
+        </p>
+      </div>
+
+      <SiteTechStackItems />
     </div>
   );
 };
