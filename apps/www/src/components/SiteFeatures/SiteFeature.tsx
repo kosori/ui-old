0f2909e74c82825808dfdd74e94343d55437f6eb
Alexis Guzman <codingcodax@gmail.com>
2023-06-15 20:43:02 -0600

    feat(components): setup site features

--- apps/www/src/components/SiteFeatures/SiteFeature.tsx
+++ apps/www/src/components/SiteFeatures/SiteFeature.tsx
@@ -1,9 +1,19 @@
-const SiteFeature = () => {
+type Props = {
+  children: React.ReactNode;
+};
+
+export const SiteFeature = ({ children }: Props) => {
   return (
-    <div>
-      <p>SiteFeature</p>
-    </div>
+    <article className='space-y-1 rounded-2xl border border-mauveLine px-4 pb-5 pt-4 shadow'>
+      {children}
+    </article>
   );
 };
 
-export default SiteFeature;
+export const SiteFeatureTitle = ({ children }: Props) => {
+  return <h3 className='text-lg font-medium'>{children}</h3>;
+};
+
+export const SiteFeatureDescription = ({ children }: Props) => {
+  return <p className='text-md text-mauveText'>{children}</p>;
+};
