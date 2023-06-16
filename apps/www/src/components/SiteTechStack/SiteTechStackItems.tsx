9cdf442c24a0a1109064613b84eee8f810b1a407
Alexis Guzman <codingcodax@gmail.com>
2023-06-16 17:36:42 -0600

    feat(components): setup site tech stack items

--- apps/www/src/components/SiteTechStack/SiteTechStackItems.tsx
+++ apps/www/src/components/SiteTechStack/SiteTechStackItems.tsx
@@ -1,7 +1,50 @@
+import {
+  Lucide,
+  RadixUI,
+  TailwindCSS,
+  TailwindVariants,
+} from '~/components/icons';
+import {
+  SiteTechStackItem,
+  SiteTechStackItemDescription,
+  SiteTechStackItemTitle,
+} from './SiteTechStackItem';
+
+const items = [
+  {
+    icon: <RadixUI />,
+    title: 'Radix UI',
+    description: 'Accessible components for exceptional design systems.',
+  },
+  {
+    icon: <TailwindCSS />,
+    title: 'Tailwind CSS',
+    description: 'Utility-first CSS framework for web development.',
+  },
+  {
+    icon: <TailwindVariants />,
+    title: 'Tailwind Variants',
+    description: 'Tailwind combined with a first-class variant API.',
+  },
+  {
+    icon: <Lucide />,
+    title: 'Lucide Icons',
+    description: 'Beautiful & consistent icon toolkit.',
+  },
+];
+
 const SiteTechStackItems = () => {
   return (
-    <div>
-      <p>SiteTechStackItems</p>
+    <div className='mx-auto grid w-full max-w-screen-lg grid-flow-col gap-x-6'>
+      {items.map(({ icon, title, description }) => (
+        <SiteTechStackItem key={title}>
+          <div className='h-12 w-12'>{icon}</div>
+          <SiteTechStackItemTitle>{title}</SiteTechStackItemTitle>
+          <SiteTechStackItemDescription>
+            {description}
+          </SiteTechStackItemDescription>
+        </SiteTechStackItem>
+      ))}
     </div>
   );
 };
