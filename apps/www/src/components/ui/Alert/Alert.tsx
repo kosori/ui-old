e4b51246ce5a0ff8b208758d075ccead0bd63c24
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 12:56:00 -0600

    feat(alert): setup root component

--- apps/www/src/components/ui/Alert/Alert.tsx
+++ apps/www/src/components/ui/Alert/Alert.tsx
@@ -1,9 +1,39 @@
-const Alert = () => {
-  return (
-    <div>
-      <p>Alert</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+import { cn } from '~/utils';
+
+const alertStyles = tv({
+  base: cn('relative shadow w-full rounded-xl bg-mauveBase border p-4'),
+  variants: {
+    intent: {
+      default: cn('text-mauveTextContrast border-mauveLine'),
+      info: cn('text-infoSolid border-infoLine'),
+      success: cn('text-successSolid border-successLine'),
+      warning: cn('text-warningSolid border-warningLine'),
+      error: cn('text-errorSolid border-errorLine'),
+    },
+  },
+  defaultVariants: {
+    intent: 'default',
+  },
+});
+
+type AlertProps = React.HTMLAttributes<HTMLDivElement>;
+type AlertVariants = VariantProps<typeof alertStyles>;
+interface Props extends AlertProps, AlertVariants { }
+
+const Alert = forwardRef<HTMLDivElement, Props>(
+  ({ className, intent, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={alertStyles({ intent, class: className })}
+      role='alert'
+      {...props}
+    />
+  ),
+);
+
+Alert.displayName = 'Alert';
 
 export default Alert;
