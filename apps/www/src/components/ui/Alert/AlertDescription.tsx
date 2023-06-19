b33fecef725a9a00580b8f2ab35cffb6c394a436
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 12:55:53 -0600

    feat(alert): setup description component

--- apps/www/src/components/ui/Alert/AlertDescription.tsx
+++ apps/www/src/components/ui/Alert/AlertDescription.tsx
@@ -1,9 +1,16 @@
-const AlertDescription = () => {
-  return (
-    <div>
-      <p>AlertDescription</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+
+import { cn } from '~/utils';
+
+type Ref = HTMLParagraphElement;
+type Props = React.HTMLAttributes<HTMLParagraphElement>;
+
+const AlertDescription = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <p ref={ref} className={cn('text-mauveText', className)} {...props} />
+  ),
+);
+
+AlertDescription.displayName = 'AlertDescription';
 
 export default AlertDescription;
