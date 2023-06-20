11aca7673c14d8fead0dea61b010b5e1cab8c780
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 20:32:26 -0600

    feat(examples): create and setup alert info component

---
+++ apps/www/src/components/examples/Alert/Info.tsx
@@ -0,0 +1,18 @@
+import { Megaphone } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/Alert';
+
+const Info = () => {
+  return (
+    <Alert intent='info'>
+      <AlertTitle>
+        <Megaphone className='mr-2 h-5 w-5' /> Account verification needed
+      </AlertTitle>
+      <AlertDescription>
+        Verify your account to unlock full access and benefits.
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+export default Info;
