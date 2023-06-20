88500fcc0ab78a28aa74489b91bd128948aa11e1
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 13:11:40 -0600

    feat(avatar): setup image component

--- apps/www/src/components/ui/Avatar/AvatarImage.tsx
+++ apps/www/src/components/ui/Avatar/AvatarImage.tsx
@@ -1,9 +1,20 @@
-const AvatarImage = () => {
-  return (
-    <div>
-      <p>AvatarImage</p>
-    </div>
-  );
-};
+import { forwardRef } from 'react';
+import { Image } from '@radix-ui/react-avatar';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Image>;
+type Props = React.ComponentPropsWithoutRef<typeof Image>;
+
+const AvatarImage = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  // eslint-disable-next-line jsx-a11y/alt-text
+  <Image
+    ref={ref}
+    className={cn('aspect-square h-full w-full', className)}
+    {...props}
+  />
+));
+
+AvatarImage.displayName = Image.displayName;
 
 export default AvatarImage;
