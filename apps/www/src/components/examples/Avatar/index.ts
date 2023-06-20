070e24c8ea96b8eca99d42f9471994aa3d1b517c
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 13:26:21 -0600

    feat(exmples): export avatar large component

--- apps/www/src/components/examples/Avatar/index.ts
+++ apps/www/src/components/examples/Avatar/index.ts
@@ -2,3 +2,4 @@ export { default as Demo } from './Demo';
 export { default as Square } from './Square';
 export { default as ExtraSmall } from './ExtraSmall';
 export { default as Medium } from './Medium';
+export { default as Large } from './Large';
