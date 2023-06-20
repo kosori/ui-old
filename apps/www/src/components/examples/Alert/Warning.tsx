5c8b817c93fde6f26f50fb4b3672c718e5b82dc2
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 20:42:56 -0600

    feat(examples): create and setup alert warning component

---
+++ apps/www/src/components/examples/Alert/Warning.tsx
@@ -0,0 +1,18 @@
+import { AlertTriangle } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/Alert';
+
+const Warning = () => {
+  return (
+    <Alert intent='warning'>
+      <AlertTitle>
+        <AlertTriangle className='mr-2 h-5 w-5' /> Exceeded storage limit
+      </AlertTitle>
+      <AlertDescription>
+        You have exceeded your storage limit. Delete files to free up space.
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+export default Warning;
