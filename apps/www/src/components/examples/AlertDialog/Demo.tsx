5e7a0f653ef559c947328c717bc21064186f6d58
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 18:18:00 -0600

    feat(examples): setup alert dialog demo

--- apps/www/src/components/examples/AlertDialog/Demo.tsx
+++ apps/www/src/components/examples/AlertDialog/Demo.tsx
@@ -1,8 +1,38 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '~/components/ui/AlertDialog';
+import { Button } from '~/components/ui/Button';
+
 const Demo = () => {
   return (
-    <div>
-      <p>AlertDialog</p>
-    </div>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button intent='tertiary'>Delete account</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Yes, delete account</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
