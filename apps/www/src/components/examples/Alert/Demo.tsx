94080e99d16fbca05dc87bab6261c5d111736224
Alexis Guzman <codingcodax@gmail.com>
2023-06-19 20:34:02 -0600

    feat(examples): setup alert demo component

--- apps/www/src/components/examples/Alert/Demo.tsx
+++ apps/www/src/components/examples/Alert/Demo.tsx
@@ -1,8 +1,17 @@
+import { AlertCircle } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/Alert';
+
 const Demo = () => {
   return (
-    <div>
-      <p>Alert</p>
-    </div>
+    <Alert>
+      <AlertTitle>
+        <AlertCircle className='mr-2 h-5 w-5' /> Heads up!
+      </AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
   );
 };
 
