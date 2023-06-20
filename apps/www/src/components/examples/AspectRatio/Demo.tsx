2d42fd58def06ecdf03edcfc41225fe320e11a90
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 09:30:12 -0600

    feat(examples): setup aspect ratio demo

--- apps/www/src/components/examples/AspectRatio/Demo.tsx
+++ apps/www/src/components/examples/AspectRatio/Demo.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
+import { AspectRatio } from '~/components/ui/AspectRatio';
+
 const Demo = () => {
   return (
-    <div>
-      <p>AspectRatio</p>
-    </div>
+    <AspectRatio ratio={16 / 9}>
+      <Image
+        fill
+        alt='a plane flying in the sky at sunset'
+        className='rounded-2xl object-cover'
+        src='https://images.unsplash.com/photo-1642680571569-a7676d19c215?ixlib=rb-4.0.3&ixid=M3wxMjA3'
+      />
+    </AspectRatio>
   );
 };
 
