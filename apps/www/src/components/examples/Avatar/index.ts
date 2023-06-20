b1c1d1f574f89beb8112c10a748a2d932d09f2c8
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 13:24:36 -0600

    feat(examples): export avatar medium component

--- apps/www/src/components/examples/Avatar/index.ts
+++ apps/www/src/components/examples/Avatar/index.ts
@@ -1,3 +1,4 @@
 export { default as Demo } from './Demo';
 export { default as Square } from './Square';
 export { default as ExtraSmall } from './ExtraSmall';
+export { default as Medium } from './Medium';
