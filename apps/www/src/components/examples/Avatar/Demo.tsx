325b1a942a17e377b52e7acfaeda7419480eecff
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 13:15:21 -0600

    feat(examples): setup avatar demo component

--- apps/www/src/components/examples/Avatar/Demo.tsx
+++ apps/www/src/components/examples/Avatar/Demo.tsx
@@ -1,9 +1,10 @@
-const Demo = () => {
-  return (
-    <div>
-      <p>Avatar</p>
-    </div>
-  );
-};
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/Avatar';
+
+const Demo = () => (
+  <Avatar>
+    <AvatarImage alt='@codingcodax' src='https://github.com/codingcodax.png' />
+    <AvatarFallback>CC</AvatarFallback>
+  </Avatar>
+);
 
 export default Demo;
