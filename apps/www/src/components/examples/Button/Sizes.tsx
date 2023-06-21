dbd9962f1971fd33de942d794979574f16955cfa
Alexis Guzman <codingcodax@gmail.com>
2023-06-20 18:10:50 -0600

    feat(examples): create and setup sizes button component

---
+++ apps/www/src/components/examples/Button/Sizes.tsx
@@ -0,0 +1,15 @@
+import { Button } from '~/components/ui/Button';
+
+const Sizes = () => {
+  return (
+    <div className='flex space-x-4'>
+      <Button size='extra-small'>Button</Button>
+      <Button size='small'>Button</Button>
+      <Button size='medium'>Button</Button>
+      <Button size='large'>Button</Button>
+      <Button size='extra-large'>Button</Button>
+    </div>
+  );
+};
+
+export default Sizes;
