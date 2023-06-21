038b00ff448aa70a0eed09db5135c260b25e414b
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:31:55 -0600

    feat(examples): add rounded shape variant

--- apps/www/src/components/examples/Checkbox/Disabled.tsx
+++ apps/www/src/components/examples/Checkbox/Disabled.tsx
@@ -3,9 +3,16 @@ import { Label } from '~/components/ui/Label';
 
 const Disabled = () => {
   return (
-    <div className='flex items-center space-x-2'>
-      <Checkbox disabled id='terms' />
-      <Label htmlFor='terms'>Accept terms and conditions</Label>
+    <div className='space-y-2'>
+      <div className='flex items-center space-x-2'>
+        <Checkbox disabled id='terms' />
+        <Label htmlFor='terms'>Accept terms and conditions</Label>
+      </div>
+
+      <div className='flex items-center space-x-2'>
+        <Checkbox disabled id='terms' shape='rounded' />
+        <Label htmlFor='terms'>Accept terms and conditions</Label>
+      </div>
     </div>
   );
 };
