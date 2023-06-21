f1815a08101fe4c72be819c81e94d620b2ec360c
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:52:46 -0600

    feat(examples): export intents badge component

--- apps/www/src/components/examples/Badge/index.ts
+++ apps/www/src/components/examples/Badge/index.ts
@@ -1,4 +1,5 @@
 export { default as Demo } from './Demo';
+export { default as Intents } from './Intents';
 export { default as Info } from './Info';
 export { default as Success } from './Success';
 export { default as Warning } from './Warning';
