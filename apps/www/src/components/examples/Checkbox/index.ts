4770eb413d0f8d521a46a15dc888fbb649d0abc1
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:32:09 -0600

    feat(examples): update exports order

--- apps/www/src/components/examples/Checkbox/index.ts
+++ apps/www/src/components/examples/Checkbox/index.ts
@@ -1,4 +1,4 @@
 export { default as Demo } from './Demo';
+export { default as Shapes } from './Shapes';
 export { default as Disabled } from './Disabled';
 export { default as Caption } from './Caption';
-export { default as Shapes } from './Shapes';
