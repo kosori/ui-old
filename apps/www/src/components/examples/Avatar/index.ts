0a46665cca38d846575a7205a669224e171d0fe8
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:48:00 -0600

    feat(examples): remove unnecessary avatar components

--- apps/www/src/components/examples/Avatar/index.ts
+++ apps/www/src/components/examples/Avatar/index.ts
@@ -1,8 +1,3 @@
 export { default as Demo } from './Demo';
 export { default as Sizes } from './Sizes';
 export { default as Shapes } from './Shapes';
-export { default as Square } from './Square';
-export { default as ExtraSmall } from './ExtraSmall';
-export { default as Medium } from './Medium';
-export { default as Large } from './Large';
-export { default as ExtraLarge } from './ExtraLarge';
