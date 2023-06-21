9bbe7683d5ba54479758f8107f2025833670d0e4
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:44:13 -0600

    feat(examples): create and setup sizes avatar component

---
+++ apps/www/src/components/examples/Avatar/Sizes.tsx
@@ -0,0 +1,47 @@
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/Avatar';
+
+const Demo = () => (
+  <div className='flex gap-x-4'>
+    <Avatar size='extra-small'>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+
+    <Avatar>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+
+    <Avatar size='medium'>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+
+    <Avatar size='large'>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+
+    <Avatar size='extra-large'>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  </div>
+);
+
+export default Demo;
