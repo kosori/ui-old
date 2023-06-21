af9621846bd40f2cd5d6481b8a2d10217f4b92c3
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:53:31 -0600

    fix(examples): update badge exports

--- apps/www/src/components/examples/Badge/index.ts
+++ apps/www/src/components/examples/Badge/index.ts
@@ -1,6 +1,2 @@
 export { default as Demo } from './Demo';
 export { default as Intents } from './Intents';
-export { default as Info } from './Info';
-export { default as Success } from './Success';
-export { default as Warning } from './Warning';
-export { default as Danger } from './Danger';
