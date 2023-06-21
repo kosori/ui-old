7b9ee4d73a2dc43ccf4ef01ca5fae9fa75b49c54
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:46:54 -0600

    feat(examples): create and setup shapes avatar component

---
+++ apps/www/src/components/examples/Avatar/Shapes.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/Avatar';
+
+const Shapes = () => (
+  <div className='flex gap-x-4'>
+    <Avatar>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+
+    <Avatar shape='square'>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  </div>
+);
+
+export default Shapes;
