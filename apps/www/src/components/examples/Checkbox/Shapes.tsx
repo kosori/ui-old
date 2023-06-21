6cee66de2cf9778eb412a5f77e6eec38122930c7
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:29:07 -0600

    feat(examples): create and setup shapes checkbox component

---
+++ apps/www/src/components/examples/Checkbox/Shapes.tsx
@@ -0,0 +1,20 @@
+import { Checkbox } from '~/components/ui/Checkbox';
+import { Label } from '~/components/ui/Label';
+
+const Caption = () => {
+  return (
+    <div className='space-y-2'>
+      <div className='flex items-center space-x-2'>
+        <Checkbox id='terms' />
+        <Label htmlFor='terms'>Accept terms and conditions</Label>
+      </div>
+
+      <div className='flex items-center space-x-2'>
+        <Checkbox id='terms-2' shape='rounded' />
+        <Label htmlFor='terms-2'>Accept terms and conditions</Label>
+      </div>
+    </div>
+  );
+};
+
+export default Caption;
