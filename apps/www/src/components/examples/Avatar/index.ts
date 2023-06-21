4f85b230f5e5aa5b2234ef656921651d02aeb9ff
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 17:44:23 -0600

    feat(examples): export sizes avatar component

--- apps/www/src/components/examples/Avatar/index.ts
+++ apps/www/src/components/examples/Avatar/index.ts
@@ -1,4 +1,5 @@
 export { default as Demo } from './Demo';
+export { default as Sizes } from './Sizes';
 export { default as Square } from './Square';
 export { default as ExtraSmall } from './ExtraSmall';
 export { default as Medium } from './Medium';
