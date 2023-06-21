e95be04c74449b1d683272095f6ee2f5c6face0a
Alexis Guzman <codingcodax@gmail.com>
2023-06-21 12:22:16 -0600

    feat(examples): setup demo checkbox

--- apps/www/src/components/examples/Checkbox/Demo.tsx
+++ apps/www/src/components/examples/Checkbox/Demo.tsx
@@ -1,9 +1,13 @@
-const Checkbox = () => {
+import { Checkbox } from '~/components/ui/Checkbox';
+import { Label } from '~/components/ui/Label';
+
+const Demo = () => {
   return (
-    <div>
-      <p>Checkbox</p>
+    <div className='flex items-center space-x-2'>
+      <Checkbox id='terms' />
+      <Label htmlFor='terms'>Accept terms and conditions</Label>
     </div>
   );
 };
 
-export default Checkbox;
+export default Demo;
