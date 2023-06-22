3d0a8035c42da2d3ef7ceaab714351e82c535ad1
Alexis Guzman <codingcodax@gmail.com>
2023-06-22 17:13:23 -0600

    feat(home): add faqs, feature and hero site components

--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from 'next';
 
-import { SiteHero } from '~/components';
+import { SiteFAQs, SiteFeatures, SiteHero, SiteTechStack } from '~/components';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +10,12 @@ const Home: NextPage = () => {
       <div className='mx-auto mt-20 flex w-full max-w-screen-xl items-end justify-center'>
         <div className='h-[300px] w-[600px] rounded-xl bg-mauveBgSubtle' />
       </div>
+
+      <SiteFeatures />
+
+      <SiteTechStack />
+
+      <SiteFAQs />
     </div>
   );
 };
