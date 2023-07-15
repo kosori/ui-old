c82b048f8f658bab1400209c15548fd1c95b0daf
Alexis Guzman <codingcodax@gmail.com>
2023-07-14 19:52:23 -0600

    feat(ui): create and setup form item component

---
+++ apps/www/src/components/ui/Form/FormItem.tsx
@@ -0,0 +1,20 @@
+import { forwardRef, useId } from 'react';
+
+import { cn } from '~/utils';
+import FormItemContext from './FormItemContext';
+
+const FormItem = forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  const id = useId();
+
+  return (
+    <FormItemContext.Provider value={{ id }}>
+      <div ref={ref} className={cn('space-y-2', className)} {...props} />
+    </FormItemContext.Provider>
+  );
+});
+FormItem.displayName = 'FormItem';
+
+export default FormItem;
