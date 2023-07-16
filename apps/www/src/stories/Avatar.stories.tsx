0189bcdeaad295a70e2c3b656db2ef81b3841fcc
Alexis Guzman <codingcodax@gmail.com>
2023-07-15 20:50:15 -0600

    feat(stories): create avatar

---
+++ apps/www/src/stories/Avatar.stories.tsx
@@ -0,0 +1,159 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/Avatar';
+
+const meta: Meta<typeof Avatar> = {
+  title: 'Example/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  argTypes: {
+    shape: {
+      control: { type: 'select' },
+      description: 'Con',
+      defaultValue: { summary: 'rounded' },
+      options: ['rounded', 'square'],
+    },
+    size: {
+      control: { type: 'select' },
+      description: '',
+      defaultValue: { summary: 'small' },
+      options: ['small', 'extra-small', 'medium', 'large', 'extra-large'],
+    },
+  },
+  args: {
+    shape: 'rounded',
+    size: 'small',
+  },
+};
+
+type Story = StoryObj<typeof Avatar>;
+
+export const Default: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'small',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const Rounded: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'small',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const Square: Story = {
+  args: {
+    shape: 'square',
+    size: 'small',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const ExtraSmall: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'extra-small',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const Small: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'small',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const Medium: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'medium',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const Large: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'large',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export const ExtraLarge: Story = {
+  args: {
+    shape: 'rounded',
+    size: 'extra-large',
+  },
+  render: ({ shape, size }) => (
+    <Avatar shape={shape} size={size}>
+      <AvatarImage
+        alt='@codingcodax'
+        src='https://github.com/codingcodax.png'
+      />
+      <AvatarFallback>CC</AvatarFallback>
+    </Avatar>
+  ),
+};
+
+export default meta;
