df7787d8ea81513284f81b584f356b82d6989c91
Alexis Guzman <codingcodax@gmail.com>
2023-07-16 13:00:38 -0600

    feat(storybook): config dark mode

--- apps/www/.storybook/preview.ts
+++ apps/www/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
 import '../src/styles/globals.css';
 
@@ -10,6 +11,14 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    darkMode: {
+      dark: { ...themes.dark, appBg: '#1c1c1f' },
+      light: { ...themes.light, appBg: '#fdfcfd' },
+      darkClass: 'dark',
+      lightClass: 'light',
+      classTarget: 'html',
+      stylePreview: true,
     },
   },
 };
