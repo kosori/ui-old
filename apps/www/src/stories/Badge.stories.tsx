c873d9b4efba98559d45672b1a673db111dfaf91
Alexis Guzman <codingcodax@gmail.com>
2023-07-16 08:19:13 -0600

    feat(stories): create badge

---
+++ apps/www/src/stories/Badge.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Badge } from '~/components/ui/Badge';
+
+const meta: Meta<typeof Badge> = {
+  title: 'Example/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    intent: {
+      control: { type: 'select' },
+      description: 'Choose the intent:',
+    },
+    children: {
+      type: 'string',
+      description: 'Write the badge text',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  args: {
+    intent: 'default',
+    children: 'Badge',
+  },
+};
+
+export const Info: Story = {
+  args: {
+    intent: 'info',
+    children: 'Badge',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    intent: 'success',
+    children: 'Badge',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    intent: 'warning',
+    children: 'Badge',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    intent: 'danger',
+    children: 'Badge',
+  },
+};
