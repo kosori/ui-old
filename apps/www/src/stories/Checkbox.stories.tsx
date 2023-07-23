6e76091f8178b5fd12a8354a8364abe96c1d31c4
Alexis Guzman <codingcodax@gmail.com>
2023-07-22 21:01:25 -0600

    feat(stories): create checkbox

---
+++ apps/www/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox, Label } from '~/components/ui';
+
+const meta: Meta<typeof Checkbox> = {
+  title: 'Example/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  argTypes: {
+    shape: {
+      control: { type: 'select' },
+      description: 'Choose the shape',
+      defaultValue: { summary: 'square' },
+      options: ['square', 'rounded'],
+    },
+    size: {
+      control: { type: 'select' },
+      description: 'Choose the size',
+      defaultValue: { summary: 'small' },
+      options: ['small', 'medium'],
+    },
+  },
+  args: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Slash: Story = {
+  args: {
+    shape: 'square',
+    size: 'small',
+  },
+  render: ({ shape, size }) => (
+    <div className='flex items-center space-x-2'>
+      <Checkbox id='terms' shape={shape} size={size} />
+      <Label htmlFor='terms'>Accept terms and conditions</Label>
+    </div>
+  ),
+};
