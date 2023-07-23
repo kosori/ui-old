09d9360cf8843c899301450598e585038e8f7849
Alexis Guzman <codingcodax@gmail.com>
2023-07-22 20:41:18 -0600

    feat(stories): add range version for calendar

--- apps/www/src/stories/Calendar.stories.tsx
+++ apps/www/src/stories/Calendar.stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import type { DayPicker } from 'react-day-picker';
+import type { DateRange, DayPicker } from 'react-day-picker';
 
 import { Calendar } from '~/components/ui';
 
@@ -37,9 +37,36 @@ const SingleCalendar = ({
   );
 };
 
+const RangeCalendar = ({
+  fixedWeeks,
+}: React.ComponentProps<typeof DayPicker>) => {
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: new Date(),
+  });
+
+  return (
+    <Calendar
+      className='rounded-2xl border border-greyLine'
+      fixedWeeks={fixedWeeks}
+      mode='range'
+      numberOfMonths={2}
+      selected={date}
+      onSelect={setDate}
+    />
+  );
+};
+
 export const Single: Story = {
   args: {
     fixedWeeks: true,
   },
   render: ({ fixedWeeks }) => <SingleCalendar fixedWeeks={fixedWeeks} />,
+};
+
+export const Range: Story = {
+  args: {
+    fixedWeeks: true,
+  },
+  render: ({ fixedWeeks }) => <RangeCalendar fixedWeeks={fixedWeeks} />,
 };
