a4554ba6a26643c3cd62a7daf43f5ff6d5db0fae
Alexis Guzman <codingcodax@gmail.com>
2023-07-25 05:57:52 -0600

    feat(stories): split checkbox into shapes and sizes

--- apps/www/src/stories/CheckboxSizes.stories.tsx
+++ apps/www/src/stories/CheckboxSizes.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Checkbox, Label } from '~/components/ui';
 
 const meta: Meta<typeof Checkbox> = {
-  title: 'Example/Checkbox',
+  title: 'Design System/Checkbox/Sizes',
   component: Checkbox,
   tags: ['autodocs'],
   argTypes: {
@@ -26,45 +26,6 @@ const meta: Meta<typeof Checkbox> = {
 export default meta;
 
 type Story = StoryObj<typeof Checkbox>;
-
-export const Default: Story = {
-  args: {
-    shape: 'square',
-    size: 'small',
-  },
-  render: ({ shape, size }) => (
-    <div className='flex items-center space-x-2'>
-      <Checkbox id='terms' shape={shape} size={size} />
-      <Label htmlFor='terms'>Accept terms and conditions</Label>
-    </div>
-  ),
-};
-
-export const Square: Story = {
-  args: {
-    shape: 'square',
-    size: 'small',
-  },
-  render: ({ shape, size }) => (
-    <div className='flex items-center space-x-2'>
-      <Checkbox id='terms' shape={shape} size={size} />
-      <Label htmlFor='terms'>Accept terms and conditions</Label>
-    </div>
-  ),
-};
-
-export const Rounded: Story = {
-  args: {
-    shape: 'rounded',
-    size: 'small',
-  },
-  render: ({ shape, size }) => (
-    <div className='flex items-center space-x-2'>
-      <Checkbox id='terms' shape={shape} size={size} />
-      <Label htmlFor='terms'>Accept terms and conditions</Label>
-    </div>
-  ),
-};
 
 export const Small: Story = {
   args: {
