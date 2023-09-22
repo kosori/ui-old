18d36183edf4db2261365f538e2c7c40f0463367
Alexis Guzman <codingcodax@gmail.com>
2023-09-22 13:21:27 -0600

    style(prettier): format index file

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,7 +1,7 @@
+import NextAuth from 'next-auth';
 import Discord from '@auth/core/providers/discord';
 import type { DefaultSession } from '@auth/core/types';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
-import NextAuth from 'next-auth';
 
 import { db, tableCreator } from '@acme/db';
 
