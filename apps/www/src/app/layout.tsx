a1136c3fc7fd7f4aa2cb6a3844166e8cbf84f689
Alexis Guzman <codingcodax@gmail.com>
2023-09-22 12:40:34 -0600

    feat(layout): remove trpc provider and update metadata

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -3,39 +3,35 @@ import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
 
-import { headers } from "next/headers";
-
-import { TRPCReactProvider } from "./providers";
-
 const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  title: "UI - codingcodax",
+  description: "Build high-quality and accessible apps in a short time.",
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    title: "UI library",
+    description: "Build high-quality and accessible apps in a short time.",
+    url: "https://ui.condingcodax.dev",
+    siteName: "UI",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+    site: "@codingcodax",
+    creator: "@codingcodax",
   },
 };
 
-export default function Layout(props: { children: React.ReactNode }) {
+const Layout = (props: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={["font-sans", fontSans.variable].join(" ")}>
-        <TRPCReactProvider headers={headers()}>
-          {props.children}
-        </TRPCReactProvider>
+        {props.children}
       </body>
     </html>
   );
-}
+};
+
+export default Layout;
