dcd07084b98c0aa180814a7e62376d84975a6092
Alexis Guzman <codingcodax@gmail.com>
2023-09-25 21:30:26 -0600

    feat(tailwind): add colors to theme

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,9 +1,12 @@
 import type { Config } from 'tailwindcss';
 
+import colors from './colors';
+
 export default {
   darkMode: 'class',
   content: [''],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
