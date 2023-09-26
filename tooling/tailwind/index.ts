90cfb02f39feeb8b0e6b7b4642e8affcdd11f2d2
Alexis Guzman <codingcodax@gmail.com>
2023-09-25 21:30:37 -0600

    feat(tailwind): add font size to theme

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,12 +1,14 @@
 import type { Config } from 'tailwindcss';
 
 import colors from './colors';
+import fontSize from './fontSize';
 
 export default {
   darkMode: 'class',
   content: [''],
   theme: {
     colors,
+    fontSize,
     extend: {},
   },
   plugins: [],
