5142a04de07da6e36e35fb6eb9dacb9488410e32
Alexis Guzman <codingcodax@gmail.com>
2023-10-01 09:06:59 -0600

    feat(ui): setup tooltip content

---
+++ apps/www/src/components/ui/Tooltip/TooltipContent.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Content } from '@radix-ui/react-tooltip';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const TooltipContent = forwardRef<Ref, Props>(
+  ({ className, sideOffset = 4, ...props }, ref) => (
+    <Content
+      ref={ref}
+      className={cn(
+        'z-50 overflow-hidden rounded-lg border border-greyLine bg-greyBase px-3 py-1.5 text-sm shadow-md',
+        className,
+      )}
+      sideOffset={sideOffset}
+      {...props}
+    />
+  ),
+);
+
+TooltipContent.displayName = Content.displayName;
+
+export default TooltipContent;
