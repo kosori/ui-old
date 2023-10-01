73e879117196c8048ea8de4e5d4d90264c5d5662
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:35:21 -0600

    feat(ui): export all hover card components

---
+++ apps/www/src/components/ui/HoverCard/index.ts
@@ -0,0 +1,4 @@
+export { default as HoverCard } from './HoverCard';
+export { default as HoverCardTrigger } from './HoverCardTrigger';
+export { default as HoverCardPortal } from './HoverCardPortal';
+export { default as HoverCardContent } from './HoverCardContent';
