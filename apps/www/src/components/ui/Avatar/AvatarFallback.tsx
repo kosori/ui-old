561d05e6a201145548d7e1de8fb44eacee13709c
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:23:15 -0600

    feat(ui): setup avatar fallback

---
+++ apps/www/src/components/ui/Avatar/AvatarFallback.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Fallback } from '@radix-ui/react-avatar';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Fallback>;
+type Props = React.ComponentPropsWithoutRef<typeof Fallback>;
+
+const AvatarFallback = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <Fallback
+      ref={ref}
+      className={cn(
+        'flex h-full w-full items-center justify-center bg-greyBgSubtle',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+AvatarFallback.displayName = Fallback.displayName;
+
+export default AvatarFallback;
