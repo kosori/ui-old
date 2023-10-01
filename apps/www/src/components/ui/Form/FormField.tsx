cf148a3e0acf581d3631c10d2c7b88812cccac84
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:33:34 -0600

    feat(ui): setup form field

---
+++ apps/www/src/components/ui/Form/FormField.tsx
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  type ControllerProps,
+  type FieldPath,
+  type FieldValues,
+} from 'react-hook-form';
+
+import FormFieldContext from './FormFieldContextValue';
+
+const FormField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  );
+};
+
+export default FormField;
