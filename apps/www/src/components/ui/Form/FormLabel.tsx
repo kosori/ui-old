723989be7c2853bb0efa38d2ada3bcb4c1521e69
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:33:58 -0600

    feat(ui): setup form label

---
+++ apps/www/src/components/ui/Form/FormLabel.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Label, type Root } from '@radix-ui/react-label';
+
+import { cn } from '~/utils';
+import useFormField from './useFormField';
+
+const FormLabel = forwardRef<
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root>
+>(({ className, ...props }, ref) => {
+  const { error, formItemId } = useFormField();
+
+  return (
+    <Label
+      ref={ref}
+      className={cn(error && 'text-errorSolid', className)}
+      htmlFor={formItemId}
+      {...props}
+    />
+  );
+});
+FormLabel.displayName = 'FormLabel';
+
+export default FormLabel;
