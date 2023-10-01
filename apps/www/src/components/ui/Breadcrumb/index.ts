2f996b7ab63bfc294ff393d220490e0a818bcc63
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:24:22 -0600

    feat(ui): export all breadcrumb components

---
+++ apps/www/src/components/ui/Breadcrumb/index.ts
@@ -0,0 +1,3 @@
+export { default as Breadcrumb } from './Breadcrumb';
+export { default as BreadcrumbContent } from './BreadcrumbContent';
+export { default as BreadcrumbSeparator } from './BreadcrumbSeparator';
