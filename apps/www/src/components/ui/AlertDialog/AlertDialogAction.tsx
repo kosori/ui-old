e3a77ea2f4d86181accb163f9d45b93744b2a102
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:19:55 -0600

    feat(ui): setup alert dialog action

---
+++ apps/www/src/components/ui/AlertDialog/AlertDialogAction.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Action } from '@radix-ui/react-alert-dialog';
+
+import {
+  buttonStyles,
+  type Props as ButtonProps,
+} from '~/components/ui/Button';
+
+type Ref = React.ElementRef<typeof Action>;
+type ActionProps = React.ComponentPropsWithoutRef<typeof Action>;
+interface Props extends ActionProps, ButtonProps { }
+
+const AlertDialogAction = forwardRef<Ref, Props>(
+  ({ intent = 'primary', size, className, ...props }, ref) => (
+    <Action
+      ref={ref}
+      className={buttonStyles({ intent, size, class: className })}
+      {...props}
+    />
+  ),
+);
+
+AlertDialogAction.displayName = Action.displayName;
+
+export default AlertDialogAction;
