04b5611efab55e19a4fc85ab1794b18c1fa56b95
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:28:26 -0600

    feat(ui): setup context menu sub content

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuSubContent.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { SubContent } from '@radix-ui/react-context-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof SubContent>;
+type Props = React.ComponentPropsWithoutRef<typeof SubContent>;
+
+const ContextMenuSubContent = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <SubContent
+      ref={ref}
+      className={cn(
+        'z-50 min-w-[8rem] overflow-hidden rounded-xl border border-greyLine bg-greyBase p-1 shadow-md',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+ContextMenuSubContent.displayName = SubContent.displayName;
+
+export default ContextMenuSubContent;
