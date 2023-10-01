cc6760cf06ba68078e072435e426b935ad76ad01
Alexis Guzman <codingcodax@gmail.com>
2023-10-01 09:07:22 -0600

    feat(ui): export all tooltip components

---
+++ apps/www/src/components/ui/Tooltip/index.ts
@@ -0,0 +1,4 @@
+export { default as Tooltip } from './Tooltip';
+export { default as TooltipProvider } from './TooltipProvider';
+export { default as TooltipTrigger } from './TooltipTrigger';
+export { default as TooltipContent } from './TooltipContent';
