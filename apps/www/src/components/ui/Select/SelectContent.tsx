9de9ab8a5e47f4fe5c8c32303e894b60599f0b09
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:45:48 -0600

    feat(ui): setup select content

---
+++ apps/www/src/components/ui/Select/SelectContent.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Content, Portal, Viewport } from '@radix-ui/react-select';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const SelectContent = forwardRef<Ref, Props>(
+  ({ className, children, position = 'popper', ...props }, ref) => (
+    <Portal>
+      <Content
+        ref={ref}
+        className={cn(
+          'relative z-50 min-w-[8rem] overflow-hidden rounded-lg border border-greyLine bg-greyBase shadow-md',
+          position === 'popper' && 'translate-y-1',
+          className,
+        )}
+        position={position}
+        {...props}
+      >
+        <Viewport
+          className={cn(
+            'p-1',
+            position === 'popper' &&
+            'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]',
+          )}
+        >
+          {children}
+        </Viewport>
+      </Content>
+    </Portal>
+  ),
+);
+
+SelectContent.displayName = Content.displayName;
+
+export default SelectContent;
