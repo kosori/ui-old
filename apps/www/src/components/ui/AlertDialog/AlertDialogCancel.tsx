cfe2dd30a869a72b578bd6e2343bf1e5441dcfa6
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:21:20 -0600

    feat(ui): setup alert dialog cancel

---
+++ apps/www/src/components/ui/AlertDialog/AlertDialogCancel.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Cancel } from '@radix-ui/react-alert-dialog';
+
+import {
+  buttonStyles,
+  type Props as ButtonProps,
+} from '~/components/ui/Button';
+
+type Ref = React.ElementRef<typeof Cancel>;
+type CancelProps = React.ComponentPropsWithoutRef<typeof Cancel>;
+interface Props extends CancelProps, ButtonProps { }
+
+const AlertDialogCancel = forwardRef<Ref, Props>(
+  ({ intent = 'secondary', size, className, ...props }, ref) => (
+    <Cancel
+      ref={ref}
+      className={buttonStyles({ intent, size, class: className })}
+      {...props}
+    />
+  ),
+);
+
+AlertDialogCancel.displayName = Cancel.displayName;
+
+export default AlertDialogCancel;
