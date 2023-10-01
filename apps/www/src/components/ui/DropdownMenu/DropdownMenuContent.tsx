ba14e92b51572ac0426b3eee522257c3a33bfdbb
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:30:57 -0600

    feat(ui): setup dropdown menu content

---
+++ apps/www/src/components/ui/DropdownMenu/DropdownMenuContent.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Content } from '@radix-ui/react-dropdown-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Content>;
+type Props = React.ComponentPropsWithoutRef<typeof Content>;
+
+const DropdownMenuContent = forwardRef<Ref, Props>(
+  ({ className, sideOffset = 4, ...props }, ref) => (
+    <Content
+      ref={ref}
+      className={cn(
+        'z-50 min-w-[8rem] overflow-hidden rounded-lg border border-greyLine bg-greyBase p-1 shadow-md',
+        className,
+      )}
+      sideOffset={sideOffset}
+      {...props}
+    />
+  ),
+);
+
+DropdownMenuContent.displayName = Content.displayName;
+
+export default DropdownMenuContent;
