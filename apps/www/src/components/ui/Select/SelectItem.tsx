eb3481f03fb37fbf9d3d28e491440147676bcd1b
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:45:58 -0600

    feat(ui): setup select item

---
+++ apps/www/src/components/ui/Select/SelectItem.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Item, ItemIndicator, ItemText } from '@radix-ui/react-select';
+import { Check } from 'lucide-react';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Item>;
+type Props = React.ComponentPropsWithoutRef<typeof Item>;
+
+const SelectItem = forwardRef<Ref, Props>(
+  ({ className, children, ...props }, ref) => (
+    <Item
+      ref={ref}
+      className={cn(
+        'relative flex h-8 w-full cursor-pointer select-none items-center rounded-md pl-8 pr-2 text-sm outline-none transition-colors duration-200',
+        'focus:bg-primaryBgHover',
+        'data-[disabled]:cursor-not-allowed data-[disabled]:text-greySolid',
+        className,
+      )}
+      {...props}
+    >
+      <span className='absolute left-2 flex items-center justify-center'>
+        <ItemIndicator>
+          <Check className='h-4 w-4' />
+        </ItemIndicator>
+      </span>
+
+      <ItemText>{children}</ItemText>
+    </Item>
+  ),
+);
+
+SelectItem.displayName = Item.displayName;
+
+export default SelectItem;
