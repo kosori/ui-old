91c5e6f1320c0c36a5f2de78d2c1ae5082944157
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:31:17 -0600

    feat(ui): setup dropdown menu label

---
+++ apps/www/src/components/ui/DropdownMenu/DropdownMenuLabel.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Label } from '@radix-ui/react-dropdown-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Label>;
+type Props = React.ComponentPropsWithoutRef<typeof Label> & { inset?: boolean };
+
+const DropdownMenuLabel = forwardRef<Ref, Props>(
+  ({ className, inset, ...props }, ref) => (
+    <Label
+      ref={ref}
+      className={cn(
+        'flex h-8 items-center px-2 text-sm font-semibold',
+        inset && 'pl-8',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+DropdownMenuLabel.displayName = Label.displayName;
+
+export default DropdownMenuLabel;
