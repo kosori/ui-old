454e986dbdc8f95b7c3127dc2169b3e095c00bdf
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:29:21 -0600

    feat(ui): setup dialog description

---
+++ apps/www/src/components/ui/Dialog/DialogDescription.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Description } from '@radix-ui/react-dialog';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Description>;
+type Props = React.ComponentPropsWithoutRef<typeof Description>;
+
+const DialogDescription = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <Description
+      ref={ref}
+      className={cn('text-sm text-greyText', className)}
+      {...props}
+    />
+  ),
+);
+
+DialogDescription.displayName = Description.displayName;
+
+export default DialogDescription;
