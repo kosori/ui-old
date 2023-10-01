79b4a23d803161bc6a32f7e7d3b03d2a9b14e465
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:28:01 -0600

    feat(ui): setup context menu separator

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuSeparator.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Separator } from '@radix-ui/react-context-menu';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof Separator>;
+type Props = React.ComponentPropsWithoutRef<typeof Separator>;
+
+const ContextMenuSeparator = forwardRef<Ref, Props>(
+  ({ className, ...props }, ref) => (
+    <Separator
+      ref={ref}
+      className={cn('-mx-2 my-1 h-px bg-greyLine', className)}
+      {...props}
+    />
+  ),
+);
+
+ContextMenuSeparator.displayName = Separator.displayName;
+
+export default ContextMenuSeparator;
