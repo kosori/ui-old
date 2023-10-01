33caf76229de51f7b04bc3eda3a5c33cb162789b
Alexis Guzman <codingcodax@gmail.com>
2023-10-01 09:05:48 -0600

    feat(ui): setup tabs list

---
+++ apps/www/src/components/ui/Tabs/TabsList.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { List } from '@radix-ui/react-tabs';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof List>;
+type Props = React.ComponentPropsWithoutRef<typeof List>;
+
+const TabsList = forwardRef<Ref, Props>(({ className, ...props }, ref) => (
+  <List
+    ref={ref}
+    className={cn(
+      'inline-flex h-10 items-center justify-center rounded-md bg-greyBg p-1 text-greyText',
+      className,
+    )}
+    {...props}
+  />
+));
+
+TabsList.displayName = List.displayName;
+
+export default TabsList;
