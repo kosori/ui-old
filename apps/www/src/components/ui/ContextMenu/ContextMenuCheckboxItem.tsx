da13eec56c930b81a43124267f7f8d10e0ba9297
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:27:16 -0600

    feat(ui): setup context menu checkbox item

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuCheckboxItem.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { CheckboxItem, ItemIndicator } from '@radix-ui/react-context-menu';
+import { Check } from 'lucide-react';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof CheckboxItem>;
+type Props = React.ComponentPropsWithoutRef<typeof CheckboxItem>;
+
+const ContextMenuCheckboxItem = forwardRef<Ref, Props>(
+  ({ className, children, checked, ...props }, ref) => (
+    <CheckboxItem
+      ref={ref}
+      checked={checked}
+      className={cn(
+        'group relative flex h-8 select-none items-center rounded-lg pl-8 pr-2 text-sm outline-none transition-colors duration-200',
+        'focus:bg-primaryBgHover',
+        'active:bg-primaryBgActive',
+        'data-[disabled]:cursor-not-allowed data-[disabled]:text-greySolid',
+        className,
+      )}
+      {...props}
+    >
+      <span className='absolute left-2 flex items-center justify-center'>
+        <ItemIndicator>
+          <Check className='h-4 w-4' />
+        </ItemIndicator>
+      </span>
+      {children}
+    </CheckboxItem>
+  ),
+);
+
+ContextMenuCheckboxItem.displayName = CheckboxItem.displayName;
+
+export default ContextMenuCheckboxItem;
