14edab01cf740966b158e353cec47ae8d666e8de
Alexis Guzman <codingcodax@gmail.com>
2023-09-30 21:28:33 -0600

    feat(ui): setup context menu sub trigger

---
+++ apps/www/src/components/ui/ContextMenu/ContextMenuSubTrigger.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { SubTrigger } from '@radix-ui/react-context-menu';
+import { ChevronRight } from 'lucide-react';
+
+import { cn } from '~/utils';
+
+type Ref = React.ElementRef<typeof SubTrigger>;
+type Props = React.ComponentPropsWithoutRef<typeof SubTrigger> & {
+  inset?: boolean;
+};
+
+const ContextMenuSubTrigger = forwardRef<Ref, Props>(
+  ({ className, inset, children, ...props }, ref) => (
+    <SubTrigger
+      ref={ref}
+      className={cn(
+        'flex h-8 select-none items-center rounded-lg px-2 text-sm outline-none transition-colors duration-200',
+        'focus:bg-primaryBgHover',
+        'data-[state=open]:bg-primaryBgHover',
+        inset && 'pl-8',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <ChevronRight className='ml-auto h-4 w-4' />
+    </SubTrigger>
+  ),
+);
+
+ContextMenuSubTrigger.displayName = SubTrigger.displayName;
+
+export default ContextMenuSubTrigger;
