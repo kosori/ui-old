b0bd996165d1b04752d4bdcb93f6abeb7f4c1e45
Alexis Guzman <codingcodax@gmail.com>
2023-10-12 20:13:45 -0700

    feat(components): setup toggle theme

--- apps/www/src/app/_components/ToggleTheme.tsx
+++ apps/www/src/app/_components/ToggleTheme.tsx
@@ -1,8 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+
 const ToggleTheme = () => {
+  const [mounted, setMouted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMouted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <div>
-      <p>toggle theme component</p>
-    </div>
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+      <option value='system'>System</option>
+      <option value='dark'>Dark</option>
+      <option value='light'>Light</option>
+    </select>
   );
 };
 
