c73adf251f250c7bcac8741514cd3cb4cba2014e
Alexis Guzman <codingcodax@gmail.com>
2023-10-15 17:38:43 -0700

    feat(stories): create separator

---
+++ apps/www/src/stories/Separator.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Separator } from '~/components/ui/Separator';
+
+const meta: Meta<typeof Separator> = {
+  title: 'Design System/Separator',
+  component: Separator,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Separator>;
+
+export const Default: Story = {
+  render: () => (
+    <div>
+      <div className='space-y-1'>
+        <h4 className='text-sm font-medium leading-none'>Radix Primitives</h4>
+        <p className='text-muted-foreground text-sm'>
+          An open-source UI component library.
+        </p>
+      </div>
+      <Separator className='my-4' />
+      <div className='flex h-5 items-center space-x-4 text-sm'>
+        <div>Blog</div>
+        <Separator orientation='vertical' />
+        <div>Docs</div>
+        <Separator orientation='vertical' />
+        <div>Source</div>
+      </div>
+    </div>
+  ),
+};
