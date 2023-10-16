85015772c570e09386d57d8990695de40c1d8791
Alexis Guzman <codingcodax@gmail.com>
2023-10-15 18:02:23 -0700

    feat(ui): export sheet title component

--- apps/www/src/components/ui/Sheet/index.ts
+++ apps/www/src/components/ui/Sheet/index.ts
@@ -2,3 +2,4 @@ export { default as Sheet } from './Sheet';
 export { default as SheetTrigger } from './SheetTrigger';
 export { default as SheetContent } from './SheetContent';
 export { default as SheetHeader } from './SheetHeader';
+export { default as SheetTitle } from './SheetTitle';
